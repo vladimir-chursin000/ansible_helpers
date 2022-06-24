d72f0caa0804236433c48279dbf5af47f2dd8d0e
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-06-25 02:38:31 +0400

    upd_helpers_playbooks_SH_and_inventory_files

--- install_nfs_server/rhel8_based/install_nfs_server.sh
+++ install_nfs_server/rhel8_based/install_nfs_server.sh
@@ -2,4 +2,4 @@
 
 SELF_DIR=`pwd -P`;
 
-ansible-playbook -i "$SELF_DIR/nfs_server_hosts" -u root --private-key=~/.ssh/id_rsa "$SELF_DIR/playbooks/install_nfs_serv_playbook.yml";
+ansible-playbook -i "$SELF_DIR/nfs_server_hosts" -u root --private-key=~/.ssh/id_rsa "$SELF_DIR/playbooks/full_install_nfs_serv_playbook.yml";
