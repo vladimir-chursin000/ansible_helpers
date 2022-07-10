4e058c63a3bc97a800cbbd31c6a60625a2615ff3
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-07-10 22:35:47 +0400

    add_for_chrony_helpers_unistall_function

---
+++ install_chrony_client/rhel8_based/uninstall_chrony_client.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+SELF_DIR="$(dirname $(readlink -f $0))";
+INV_FILE="$SELF_DIR/chrony_client_hosts";
+PLAYBOOK='uninstall_chrony_cli_playbook.yml';
+LOG_DIR="$SELF_DIR/run_history";
+
+$SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR";
