7362d156f6cd2242a6ea299b67ed8721db51e261
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-07-13 23:40:35 +0400

    corr install_nfs_server.sh

--- install_nfs_server/rhel8_based/install_nfs_server.sh
+++ install_nfs_server/rhel8_based/install_nfs_server.sh
@@ -6,5 +6,4 @@ PLAYBOOK='full_install_nfs_serv_playbook.yml';
 LOG_DIR="$SELF_DIR/run_history";
 IS_NEED_RUN_GEN_DYN_EXPORTS="YES";
 
-$SELF_DIR/generate_dynamic_exports.sh;
 $SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR" "$IS_NEED_RUN_GEN_DYN_EXPORTS";
