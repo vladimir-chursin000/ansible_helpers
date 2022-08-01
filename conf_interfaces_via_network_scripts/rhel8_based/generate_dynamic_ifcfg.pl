3743bce5e40a4663aef938ae7eab1bd60da00df3
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-08-02 00:01:41 +0400

    202208020001

--- conf_interfaces_via_network_scripts/rhel8_based/generate_dynamic_ifcfg.pl
+++ conf_interfaces_via_network_scripts/rhel8_based/generate_dynamic_ifcfg.pl
@@ -15,22 +15,24 @@ our $line_g=undef;
 our $common_uniq_criteria_yes_g=0;
 our ($inv_host_g,$conf_id_g,$conf_type_g,$int_list_str_g,$hwaddr_list_str_g,$vlan_id_g,$bond_name_g,$brigde_name_g,$ipaddr_opts_g,$bond_opts_g)=(undef,undef,undef,undef,undef,undef,undef,undef,undef,undef);
 our %cfg0_hash_g=();
-#our %cfg0_uniq_interface_check_g=();
-#our %cfg0_novlan_uniq_hwaddr_check_g=();
-#our %cfg0_novlan_uniq_vlan_check_g=();
-#our %cfg0_novlan_uniq_bondname_check_g=();
-#our %cfg0_novlan_uniq_bridge_name_check_g=();
-#our %cfg0_novlan_iniq_ip_check_g=();
 ######
 our %cfg0_uniq_check=();
-#$cfg0_uniq_check{'common'}{interface_name}=1; #if interface_name ne 'no'
-#$cfg0_uniq_check{'common'}{hwaddr}=1;
-#$cfg0_uniq_check{'common'}{bond_name}=1; #if bond_name ne 'no'
-#$cfg0_uniq_check{'common'}{bridge_name}=1; #if bridge_name ne 'no'
-#$cfg0_uniq_check{'common'}{ipaddr}=1;
+#Checks (uniq) for novlan interfaces at current inv_host.
+#$cfg0_uniq_check{inv_host}{'common'}{interface_name}=1; #if interface_name ne 'no' and vlan_id eq 'no'.
+#$cfg0_uniq_check{inv_host}{'common'}{hwaddr}=1; if vlan_id eq 'no'.
+#$cfg0_uniq_check{inv_host}{'common'}{bond_name}=1; #if bond_name ne 'no' and vlan_id eq 'no'.
+#$cfg0_uniq_check{inv_host}{'common'}{bridge_name}=1; #if bridge_name ne 'no' and vlan_id eq 'no'.
+#$cfg0_uniq_check{inv_host}{'common'}{ipaddr}=1; #if ipaddr_opts ne 'dhcp'.
 ###
-#$cfg0_uniq_check{'vlan'}{interface_name+}=1;
-#$cfg0_uniq_check{'vlan'}{vlan_id}=1;
+#Checks (uniq) for vlan interfaces at current inv_host.
+#$cfg0_uniq_check{inv_host}{'vlan'}{vlan_id}=1; #if vlan_id ne 'no'.
+#$cfg0_uniq_check{inv_host}{'vlan'}{interface_name+vlan_id}=1; #if vlan_id ne 'no'.
+#$cfg0_uniq_check{inv_host}{'vlan'}{hwaddr+vlan_id}=1; #if vlan_id ne 'no'.
+#$cfg0_uniq_check{inv_host}{'vlan'}{bond_name+vlan_id}=1; #if vlan_id ne 'no' and bond_name ne 'no'.
+#$cfg0_uniq_check{inv_host}{'vlan'}{bridge_name+vlan_id}=1; #if vlan_id ne 'no' and bridge_name ne 'no'.
+###
+#Checks (uniq) for interfaces at config (for all inv_hosts)
+
 ######
 our %cfg_ready_hash_g=();
 ############VARS
