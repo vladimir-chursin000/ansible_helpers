42eeecaa354157b7785c60edcba16e2f49fdff72
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-08-22 01:16:38 +0400

    202208220115_dobrou_noc

--- conf_int_ipv4_via_network_scripts/rhel8_based/check_network_scripts_serv_is_started.sh
+++ conf_int_ipv4_via_network_scripts/rhel8_based/check_network_scripts_serv_is_started.sh
@@ -2,7 +2,7 @@
 
 SELF_DIR="$(dirname $(readlink -f $0))";
 INV_FILE="$SELF_DIR/conf_network_scripts_hosts";
-PLAYBOOK='full_install_network_scripts_and_configure_network_playbook.yml';
+PLAYBOOK='check_network_scripts_serv_is_started_playbook.yml';
 LOG_DIR="$SELF_DIR/run_history";
 
 $SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR";
