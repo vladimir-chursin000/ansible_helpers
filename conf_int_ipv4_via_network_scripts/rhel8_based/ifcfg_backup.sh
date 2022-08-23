67a1713400cdae22cab2300e71762e2b78a9ca70
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-08-24 03:46:27 +0400

    202208240345_add_ifcfg_backup_playbook

---
+++ conf_int_ipv4_via_network_scripts/rhel8_based/ifcfg_backup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+SELF_DIR="$(dirname $(readlink -f $0))";
+INV_FILE="$SELF_DIR/conf_network_scripts_hosts";
+PLAYBOOK='ifcfg_backup_playbook.yml';
+LOG_DIR="$SELF_DIR/run_history";
+
+$SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR";
