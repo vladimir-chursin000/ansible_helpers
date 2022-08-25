731fcdcc154d1b5493b423f94d55f87e5618034f
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-08-26 01:32:47 +0400

    202208260131

--- conf_int_ipv4_via_network_scripts/rhel8_based/apply_immediately_ifcfg.sh
+++ conf_int_ipv4_via_network_scripts/rhel8_based/apply_immediately_ifcfg.sh
@@ -2,7 +2,7 @@
 
 SELF_DIR="$(dirname $(readlink -f $0))";
 INV_FILE="$SELF_DIR/conf_network_scripts_hosts";
-PLAYBOOK='check_network_scripts_conf_is_changed_playbook.yml';
+PLAYBOOK='apply_now_ifcfg_playbook.yml';
 LOG_DIR="$SELF_DIR/run_history";
 
 $SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR";
