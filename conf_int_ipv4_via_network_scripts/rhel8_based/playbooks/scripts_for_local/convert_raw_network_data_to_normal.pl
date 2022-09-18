049988250a71bd8b36bb79e9963385bc12606f2d
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-09-18 04:24:23 +0400

    202209180422

--- conf_int_ipv4_via_network_scripts/rhel8_based/playbooks/scripts_for_local/convert_raw_network_data_to_normal.pl
+++ conf_int_ipv4_via_network_scripts/rhel8_based/playbooks/scripts_for_local/convert_raw_network_data_to_normal.pl
@@ -8,8 +8,17 @@ use Data::Dumper;
 our ($self_dir_g,$script_name_g)=Cwd::abs_path($0)=~/(.*[\/\\])(\S+)$/;
 
 ###ARGV
-our $raw_data_dir='empty';
+our $raw_data_dir_g='empty';
 if ( defined($ARGV[0]) ) {
-    $raw_data_dir=$ARGV[0];
+    $raw_data_dir_g=$ARGV[0];
 }
+
+if ( $raw_data_dir_g eq 'empty' ) { exit; }
 ###ARGV
+
+###VARS
+our @ip_list_files_g=(`ls $raw_data_dir_g | grep ip_link_noqueue`);
+our @ip_neighbour_files_g=(`ls $raw_data_dir_g | grep ip_neighbour`);
+chomp(@ip_list_files_g);
+chomp(@ip_neighbour_files_g);
+###VARS
