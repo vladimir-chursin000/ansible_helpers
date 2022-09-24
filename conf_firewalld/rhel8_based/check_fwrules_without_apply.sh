e41e43661136c46bb7334e21728edb4dac8fa33a
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-09-25 03:38:27 +0400

    202209250336

---
+++ conf_firewalld/rhel8_based/check_fwrules_without_apply.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+SELF_DIR="$(dirname $(readlink -f $0))";
+INV_FILE="$SELF_DIR/conf_firewall_hosts";
+PLAYBOOK='no';
+LOG_DIR="$SELF_DIR/run_history";
+PLAYBOOK_BEFORE='fwrules_backup_playbook.yml';
+GEN_DYN_FWRULES_RUN='yes';
+
+$SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR" "$PLAYBOOK_BEFORE" "$GEN_DYN_FWRULES_RUN";
---
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+###SCRIPT for generate firewall rules for each inventory host
+
+use strict;
+use warnings;
+use Cwd;
+use Data::Dumper;
+
+our ($self_dir_g,$script_name_g)=Cwd::abs_path($0)=~/(.*[\/\\])(\S+)$/;
