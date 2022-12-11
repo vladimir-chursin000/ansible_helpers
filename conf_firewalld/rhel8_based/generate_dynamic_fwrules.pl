8855308e0011373df8eb7df911dd6529f9336784
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2022-12-12 01:05:00 +0400

    202212120101

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -46,15 +46,127 @@ our $f08_conf_policies_FIN_path_g=$self_dir_g.'/fwrules_configs/08_conf_policies
 
 ############VARS
 our %h00_conf_firewalld_hash_g=();
+#[firewall_conf_for_all--TMPLT:BEGIN]
+#host_list_for_apply=all
+#unconfigured_custom_firewall_zones_action=no_action
+#DefaultZone=public
+#CleanupOnExit=yes
+#CleanupModulesOnExit=yes
+#Lockdown=no
+#IPv6_rpfilter=yes
+#IndividualCalls=no
+#LogDenied=off
+#enable_logging_of_dropped_packets=no
+#FirewallBackend=nftables
+#FlushAllOnReload=yes
+#RFC3964_IPv4=yes
+#AllowZoneDrifting=no
+#[firewall_conf_for_all--TMPLT:END]
+######
+
+######
 our %h01_conf_ipset_templates_hash_g=();
+#[some_ipset_template_name--TMPLT:BEGIN]
+#ipset_name=some_ipset_name
+#ipset_description=empty
+#ipset_short_description=empty
+#ipset_create_option_timeout=0
+#ipset_create_option_hashsize=1024
+#ipset_create_option_maxelem=65536
+#ipset_create_option_family=inet
+#ipset_type=some_ipset_type
+#inv_host_list_for_apply=
+#[some_ipset_template_name--TMPLT:END]
+######
+
+######
 our %h02_conf_custom_firewall_zones_templates_hash_g=();
+#[some_zone--TMPLT:BEGIN]
+#zone_name=some_zone--custom
+#zone_description=
+#zone_short_description=
+#zone_target=
+#zone_allowed_services=
+#zone_allowed_ports=
+#zone_allowed_protocol_list=
+#zone_forward=
+#zone_masquerade_general=
+#zone_allowed_source_ports=
+#zone_icmp_block_inversion=
+#zone_icmp_block=
+#[some_zone--TMPLT:END]
+######
+
+######
 our %h02_conf_standard_firewall_zones_templates_hash_g=();
+#[public--TMPLT:BEGIN]
+#zone_name=public
+#zone_target=default
+#zone_allowed_services=cockpit,dhcpv6-client,ssh
+#zone_allowed_ports=empty
+#zone_allowed_protocol_list=empty
+#zone_forward=no
+#zone_masquerade_general=no
+#zone_allowed_source_ports=empty
+#zone_icmp_block_inversion=no
+#zone_icmp_block=empty
+#[public--TMPLT:END]
+######
+
+######
 our %h03_conf_policy_templates_hash_g=();
+#[some_policy--TMPLT:BEGIN]
+#policy_name=some_policy_name
+#policy_description=empty
+#policy_short_description=empty
+#policy_target=CONTINUE
+#policy_priority=-1
+#policy_allowed_services=empty
+#policy_allowed_ports=empty
+#policy_allowed_protocol_list=empty
+#policy_masquerade_general=no
+#policy_allowed_source_ports=empty
+#policy_icmp_block=empty
+#[some_policy--TMPLT:END]
+######
+
+######
 our %h04_conf_zone_forward_ports_sets_hash_g=();
+#[some_forward_ports_set_name:BEGIN]
+#port=80:proto=tcp:toport=8080:toaddr=192.168.1.60 (example)
+#port=80:proto=tcp:toport=8080 (example)
+#[some_forward_ports_set_name:END]
+######
+
+######
 our %h05_conf_zone_rich_rules_sets_hash_g=();
+#[some_rich_rules_set_name:BEGIN]
+#rule family=ipv4 forward-port to-port=8080 protocol=tcp port=80 (example)
+#rule family=ipv4 source address=192.168.55.4/32 destination address=10.10.7.0/24 masquerade (example)
+#[some_rich_rules_set_name:END]
+######
+
+######
 our %h06_conf_ipsets_FIN_hash_g=();
+#INVENTORY_HOST         #IPSET_TEMPLATE_NAME_LIST
+#all                    ipset1--TMPLT,ipset4all_public--TMPLT (example)
+#10.3.2.2               ipset4public--TMPLT (example)
+######
+
+######
 our %h07_conf_zones_FIN_hash_g=();
+#INVENTORY_HOST         #FIREWALL_ZONE_NAME_TMPLT       #INTERFACE_LIST   #SOURCE_LIST                                  #FORWARD_PORTS_SET      #RICH_RULES_SET
+#all                    public--TMPLT                   ens1,ens2,ens3    10.10.16.0/24,ipset:ipset4all_public--TMPLT   empty                   empty (example)
+#10.3.2.2               public--TMPLT                   empty             10.10.15.0/24,ipset:ipset4public--TMPLT       fw_ports_set4public     rich_rules_set4public (example)
+#10.1.2.3,10.1.2.4      zone1--TMPLT                    eth0,eth1,ens01   empty                                         fw_ports_set4zone1      rich_rules_set4zone1 (example)
+######
+
+######
 our %h08_conf_policies_FIN_hash_g=();
+#INVENTORY_HOST         #POLICY_TEMPLATE_NAME           #INGRESS-FIREWALL_ZONE_TEMPLATE_NAME    #EGRESS-FIREWALL_ZONE_TEMPLATE_NAME     #FORWARD_PORTS_SET      #RICH_RULES_SET
+#all                    policy_public2home--TMPLT       public--TMPLT                           home--TMPLT                             fw_ports_set1           rich_rules_set1 (example)
+#10.3.2.2               policy_zoneone2zonetwo--TMPLT   zoneone--TMPLT                          zonetwo--TMPLT                          fw_ports_set2           rich_rules_set2 (example)
+######
 ############VARS
 
 ############MAIN SEQ
