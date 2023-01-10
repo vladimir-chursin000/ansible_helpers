720c8a9d58444782f30b1e2b7abda8f31aa7165f
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-01-10 22:27:47 +0400

    202301102226

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -1167,6 +1167,24 @@ sub read_05_conf_zone_rich_rules_sets {
     #$file_l,$res_href_l
     if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
     
+    # check rules with regex (for future functional)
+    #while ( ($hkey0_l,$hval0_l)=each %res_tmp_lv0_l ) { # cycle 0
+    #	#hkey0_l=tmplt_name, hval0_l=hash ref where key=rule
+    #	###
+    #	while ( ($hkey1_l,$hval1_l)=each %{$hval0_l} ) { # cycle 1
+    #	    #hkey1_l=rule
+    #	    #rule family=ipv4 forward-port to-port=8080 protocol=tcp port=80 (example)
+    #	    #rule family=ipv4 source address=192.168.55.4/32 destination address=10.10.7.0/24 masquerade (example)
+    #	    if ( $hkey1_l!~/^seq$/ ) {
+    #	    }
+    #	} # cycle 1
+    #	
+    #	if ( $return_str_l!~/^OK$/ ) { last; }
+    #} # cycle 0
+    #
+    #if ( $return_str_l!~/^OK$/ ) { return $return_str_l; }
+    ###
+
     # fill result hash
     %{$res_href_l}=%res_tmp_lv0_l;
     ###
