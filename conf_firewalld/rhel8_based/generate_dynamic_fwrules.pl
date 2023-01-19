f1dc8481ae7f2c8cde98ab65baa107feca8520ce
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-01-20 00:37:53 +0400

    202301200036

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -1637,11 +1637,29 @@ sub read_88_conf_policies_FIN {
 	###
 	
 	# FORWARD_PORTS_SET ops [3]
-	
+	if ( ${$hval0_l}[3]=~/^empty$/ ) { $res_tmp_lv1_l{$inv_host_l}{'forward_ports_set'}='empty'; }
+        else {
+            #$fw_ports_set_href_l=hash-ref for %h04_conf_zone_forward_ports_sets_hash_g
+                #$h04_conf_zone_forward_ports_sets_hash_g{set_name}-> ...
+            if ( !exists(${$fw_ports_set_href_l}{${$hval0_l}[3]}) ) {
+                $return_str_l="fail [$proc_name_l]. FORWARD_PORTS_SET='${$hval0_l}[3]' (conf='$file_l) is not exists at '04_conf_zone_forward_ports_sets'";
+                last;
+            }
+            else { $res_tmp_lv1_l{$inv_host_l}{'forward_ports_set'}=${$hval0_l}[3]; }
+        }
 	###
 	
 	# RICH_RULES_SET ops [4]
-	
+	if ( ${$hval0_l}[4]=~/^empty$/ ) { $res_tmp_lv1_l{$inv_host_l}{'rich_rules_set'}='empty'; }
+	else {
+	    #$rich_rules_set_href_l=hash-ref for %h05_conf_zone_rich_rules_sets_hash_g
+    		#$h05_conf_zone_rich_rules_sets_hash_g{set_name}-> ...
+	    if ( !exists(${$rich_rules_set_href_l}{${$hval0_l}[4]}) ) {
+		$return_str_l="fail [$proc_name_l]. RICH_RULES_SET='${$hval0_l}[4]' (conf='$file_l) is not exists at '05_conf_zone_rich_rules_sets'";
+		last;
+	    }
+	    else { $res_tmp_lv1_l{$inv_host_l}{'rich_rules_set'}=${$hval0_l}[4]; }
+	}
 	###
     }
     
