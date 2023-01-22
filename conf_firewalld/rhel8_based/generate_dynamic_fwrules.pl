0c0ff7f536daf90c0b3fe48d1f786a3fd0802431
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-01-22 22:04:34 +0400

    202301222202

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -1814,7 +1814,7 @@ sub generate_zones_create_scripts {
     #{'ipset_create_option_maxelem'}=num
     #{'ipset_create_option_family'}=inet|inet6
     #{'ipset_type'}=hash:ip|hash:ip,port|hash:ip,mark|hash:net|hash:net,port|hash:net,iface|hash:mac|hash:ip,port,ip|hash:ip,port,net|hash:net,net|hash:net,port,net
-    ###
+    ######
     #$h02_conf_custom_firewall_zones_templates_hash_g{zone_teplate_name--TMPLT}->
     #{'zone_name'}=some_zone--custom
     #{'zone_description'}=empty|value
@@ -1859,9 +1859,50 @@ sub generate_zones_create_scripts {
     	    #etc
 	#{'seq'}=[val-0,val-1] (val=icmptype)
 
-    ###
-    
-    ###
+    ######
+    #$h02_conf_standard_firewall_zones_templates_hash_g{zone_teplate_name--TMPLT}->
+    #{'zone_name'}=std_zone_name
+    #{'zone_target'}=ACCEPT|REJECT|DROP|default
+    #{'zone_allowed_services'}->
+        #{'empty'}=1 or
+        #{'list'}->
+            #{'service-0'}=1
+            #{'service-1'}=1
+            #etc
+        #{'seq'}=[val-0,val-1] (val=service)
+    #{'zone_allowed_ports'}->
+        #{'empty'}=1 or
+        #{'list'}->
+            #{'port-0'}=1
+            #{'port-1'}=1
+            #etc
+        #{'seq'}=[val-0,val-1] (val=port)
+    #{'zone_allowed_protocols'}->
+        #{'empty'}=1 or
+        #{'list'}->
+            #{'proto-0'}=1
+            #{'proto-1'}=1
+            #etc
+        #{'seq'}=[val-0,val-1] (val=proto)
+    #{'zone_forward'}=yes|no
+    #{'zone_masquerade_general'}=yes|no
+    #{'zone_allowed_source_ports'}->
+        #{'empty'}=1 or
+        #{'list'}->
+            #{'port-0'}=1
+            #{'port-1'}=1
+            #etc
+        #{'seq'}=[val-0,val-1] (val=port)
+    #{'zone_icmp_block_inversion'}=yes|no
+    #{'zone_icmp_block'}->
+        #{'empty'}=1 or
+        #{'list'}->
+            #{'icmptype-0'}=1
+            #{'icmptype-1'}=1
+            #etc
+        #{'seq'}=[val-0,val-1] (val=icmptype)
+
+    ######
     #$h77_conf_zones_FIN_hash_g{inventory_host}{'custom/standard'}{firewall_zone_name_tmplt}->
     #{'interface_list'}->;
 	#{'empty'}=1
@@ -1886,7 +1927,7 @@ sub generate_zones_create_scripts {
 	#{'seq'}=[val-0,val-1] (val=ipset_tmplt)
     #{'forward_ports_set'}=empty|fw_ports_set (FROM '04_conf_zone_forward_ports_sets')
     #{'rich_rules_set'}=empty|rich_rules_set (FROM '05_conf_zone_rich_rules_sets')
-    ###
+    ######
 
     my ($exec_res_l)=(undef);
     my ($hkey0_l,$hval0_l)=(undef,undef);
