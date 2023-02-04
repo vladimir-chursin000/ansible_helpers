78950fa06e01927a7f1592a4bb9480da2f5a76c7
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-02-04 22:02:55 +0400

    202302042201

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -697,7 +697,7 @@ sub read_network_data_for_checks {
     my $proc_name_l=(caller(0))[3];
         
     my ($line_l,$value_cnt_l)=(undef,0);
-    my @arr0_l=undef;
+    my @arr0_l=();
 
     if ( length($file_l)<1 or ! -e($file_l) ) { return "fail [$proc_name_l]. File='$file_l' is not exists"; }
 
@@ -733,11 +733,67 @@ sub read_00_conf_divisions_for_inv_hosts {
     #inv_hosts_href_l=hash-ref for %inventory_hosts_g
     #res_href_l=hash-ref for %h00_conf_divisions_for_inv_hosts_hash_g
     my $proc_name_l=(caller(0))[3];
-    
-    #DIVISION_NAME/GROUP_NAME       #LIST_OF_HOSTS
-    ###
-    #$h00_conf_divisions_for_inv_hosts_hash_g{group_name}{inv-host}=1;
 
+    my ($line_l,$arr_el0_l)=(undef,undef);
+    my @arr0_l=();
+    my @arr1_l=();
+    my %group_uniq_hosts_l=();
+	#key0=inv-host, value=group_name
+    my $return_str_l='OK';
+
+    if ( length($file_l)<1 or ! -e($file_l) ) { return "fail [$proc_name_l]. File='$file_l' is not exists"; }
+    
+    open(GROUP_HOSTS,'<',$file_l);
+    while ( <GROUP_HOSTS> ) {
+        $line_l=$_;
+        $line_l=~s/\n$|\r$|\n\r$|\r\n$//g;
+        while ($line_l=~/\t/) { $line_l=~s/\t/ /g; }
+        $line_l=~s/\s+/ /g;
+        $line_l=~s/^ //g;
+	$line_l=~s/ $//g;
+	
+	$line_l=~s/\, /\,/g;
+	$line_l=~s/ \,/\,/g;
+
+        if ( length($line_l)>0 && $line_l!~/^\#/ ) {
+	    #DIVISION_NAME/GROUP_NAME       #LIST_OF_HOSTS
+	    ###
+	    #$h00_conf_divisions_for_inv_hosts_hash_g{group_name}{inv-host}=1;
+	    ###
+            @arr0_l=split(' ',$line_l);
+	    @arr1_l=split(',',$arr0_l[1]);
+	    foreach $arr_el0_l ( @arr1_l ) {
+		#$arr0_l[0]=group-name
+		#$arr_el0_l=inv-host
+		
+		if ( !exists(${$inv_hosts_href_l}{$arr_el0_l}) ) {
+		    $return_str_l="fail [$proc_name_l]. Inv-host='$arr_el0_l' is not exists at inventory-file";
+		    last;
+		}
+		
+		if ( exists($group_uniq_hosts_l{$arr_el0_l}) ) {
+		    $return_str_l="fail [$proc_name_l]. Inv-host='$arr_el0_l' is already used for host_group='$group_uniq_hosts_l{$arr_el0_l}' at conf '00_conf_divisions_for_inv_hosts'";
+		    last;
+		}
+		
+		$group_uniq_hosts_l{$arr_el0_l}=$arr0_l[0];
+		${$res_href_l}{$arr0_l[0]}{$arr_el0_l}=1;
+	    }
+	    
+	    $arr_el0_l=undef;
+	    @arr0_l=();
+	    @arr1_l=();
+	    
+	    if ( $return_str_l!~/^OK$/ ) { last; }
+        }
+    }
+    close(GROUP_HOSTS);
+    
+    $line_l=undef;
+    
+    if ( $return_str_l!~/OK$/ ) { return $return_str_l; }
+
+    return $return_str_l;
 }
 
 sub read_00_conf_firewalld {
