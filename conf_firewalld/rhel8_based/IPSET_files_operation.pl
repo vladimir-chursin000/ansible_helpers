bc50f73c13e2c4ab746b15e1ec879032a1b3b60f
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-03-03 22:21:52 +0400

    202303032219

--- conf_firewalld/rhel8_based/IPSET_files_operation.pl
+++ conf_firewalld/rhel8_based/IPSET_files_operation.pl
@@ -18,6 +18,8 @@ if ( defined($ARGV[0]) && length($ARGV[0])>0 ) {
 ############ARGV
 
 ############CFG file
+our $conf_temp_apply_g=$self_dir_g.'/additional_configs/config_temporary_apply_fwrules';
+our $f00_conf_divisions_for_inv_hosts_path_g=$self_dir_g.'/fwrules_configs/00_conf_divisions_for_inv_hosts';
 our $f01_conf_ipset_templates_path_g=$self_dir_g.'/fwrules_configs/01_conf_ipset_templates';
 our $f66_conf_ipsets_FIN_path_g=$self_dir_g.'/fwrules_configs/66_conf_ipsets_FIN';
 ############CFG file
@@ -29,6 +31,59 @@ our $dyn_fwrules_files_dir_g=$scripts_for_remote_dir_g.'/fwrules_files'; # dir f
 ############STATIC VARS
 
 ############VARS
+######
+our %inv_hosts_tmp_apply_fwrules_g=(); #key=inv_host/common, value=rollback_ifcfg_timeout
+######
+
+######
+our %inventory_hosts_g=(); # for checks of h00_conf_firewalld_hash_g/h66_conf_ipsets_FIN_hash_g/h77_conf_zones_FIN_hash_g/h88_conf_policies_FIN_hash_g
+# and operate with 'all' (apply for all inv hosts) options
+###
+#Key=inventory_host, value=1
+######
+
+######
+our %h00_conf_divisions_for_inv_hosts_hash_g=();
+#DIVISION_NAME/GROUP_NAME       #LIST_OF_HOSTS
+###
+#$h00_conf_divisions_for_inv_hosts_hash_g{group_name}{inv-host}=1;
+######
+
+######
+our %h01_conf_ipset_templates_hash_g=();
+#[some_ipset_template_name--TMPLT:BEGIN]
+#ipset_name=some_ipset_name
+#ipset_description=empty
+#ipset_short_description=empty
+#ipset_create_option_timeout=0
+#ipset_create_option_hashsize=1024
+#ipset_create_option_maxelem=65536
+#ipset_create_option_family=inet
+#ipset_type=some_ipset_type
+#[some_ipset_template_name--TMPLT:END]
+###
+#$h01_conf_ipset_templates_hash_g{'temporary/permanent'}{ipset_template_name--TMPLT}->
+#{'ipset_name'}=value
+#{'ipset_description'}=empty|value
+#{'ipset_short_description'}=empty|value
+#{'ipset_create_option_timeout'}=num
+#{'ipset_create_option_hashsize'}=num
+#{'ipset_create_option_maxelem'}=num
+#{'ipset_create_option_family'}=inet|inet6
+#{'ipset_type'}=hash:ip|hash:ip,port|hash:ip,mark|hash:net|hash:net,port|hash:net,iface|hash:mac|hash:ip,port,ip|hash:ip,port,net|hash:net,net|hash:net,port,net
+######
+
+######
+our %h66_conf_ipsets_FIN_hash_g=();
+#INVENTORY_HOST         #IPSET_NAME_TMPLT_LIST
+#all                    ipset1--TMPLT,ipset4all_public--TMPLT (example)
+#10.3.2.2               ipset4public--TMPLT (example)
+###
+#$h66_conf_ipsets_FIN_hash_g{'temporary/permanent'}{inventory_host}->
+    #{ipset_name_tmplt-0}=1;
+    #{ipset_name_tmplt-1}=1;
+    #etc
+######
 ############VARS
 
 ############MAIN SEQ
