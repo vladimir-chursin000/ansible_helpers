9b1fed84d28761178aba3631ab8047c118f45c5c
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-03-05 02:33:47 +0400

    202303050231

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -624,8 +624,8 @@ while ( 1 ) { # ONE RUN CYCLE begin
 
     ######
     
-    $exec_res_g=&generate_shell_script_for_recreate_ipsets($dyn_fwrules_files_dir_g,$remote_dir_for_absible_helper_g,\%h01_conf_ipset_templates_hash_g,\%h66_conf_ipsets_FIN_hash_g);
-    #$dyn_fwrules_files_dir_l,$ipset_templates_href_l,$h66_conf_ipsets_FIN_href_l
+    $exec_res_g=&generate_shell_script_for_recreate_ipsets($dyn_fwrules_files_dir_g,$remote_dir_for_absible_helper_g,\%inventory_hosts_g,\%h01_conf_ipset_templates_hash_g,\%h66_conf_ipsets_FIN_hash_g);
+    #$dyn_fwrules_files_dir_l,$inv_hosts_href_l,$ipset_templates_href_l,$h66_conf_ipsets_FIN_href_l
     if ( $exec_res_g=~/^fail/ ) {
         $exec_status_g='FAIL';
         print "$exec_res_g\n";
@@ -636,6 +636,7 @@ while ( 1 ) { # ONE RUN CYCLE begin
     ######
     
     %input_hash4proc_g=(
+	'inventory_hosts_href'=>\%inventory_hosts_g,
     	'h00_conf_firewalld_href'=>\%h00_conf_firewalld_hash_g,
     	'h01_conf_ipset_templates_href'=>\%h01_conf_ipset_templates_hash_g,
     	'h02_conf_standard_firewall_zones_templates_href'=>\%h02_conf_standard_firewall_zones_templates_hash_g,
@@ -657,6 +658,7 @@ while ( 1 ) { # ONE RUN CYCLE begin
     ######
     
     %input_hash4proc_g=(
+	'inventory_hosts_href'=>\%inventory_hosts_g,
     	'h01_conf_ipset_templates_href'=>\%h01_conf_ipset_templates_hash_g,
     	'h02_conf_standard_firewall_zones_templates_href'=>\%h02_conf_standard_firewall_zones_templates_hash_g,
     	'h02_conf_custom_firewall_zones_templates_href'=>\%h02_conf_custom_firewall_zones_templates_hash_g,
@@ -2361,9 +2363,10 @@ sub generate_firewall_configs {
 }
 
 sub generate_shell_script_for_recreate_ipsets {
-    my ($dyn_fwrules_files_dir_l,$remote_dir_for_absible_helper_l,$ipset_templates_href_l,$h66_conf_ipsets_FIN_href_l)=@_;
+    my ($dyn_fwrules_files_dir_l,$remote_dir_for_absible_helper_l,$inv_hosts_href_l,$ipset_templates_href_l,$h66_conf_ipsets_FIN_href_l)=@_;
     #$dyn_fwrules_files_dir_l=$dyn_fwrules_files_dir_g
     #$remote_dir_for_absible_helper_l=$remote_dir_for_absible_helper_g
+    #$inv_hosts_href_l=hash-ref for %inventory_hosts_g
     #$ipset_templates_href_l=hash-ref for %h01_conf_ipset_templates_hash_g
     #$conf_ipsets_href_l=hash ref for %h66_conf_ipsets_FIN_hash_g
     my $proc_name_l=(caller(0))[3];
@@ -2499,38 +2502,37 @@ sub generate_shell_script_for_recreate_ipsets {
     ###
     
     # create scripts for copy to remote hosts
-    while ( ($hkey0_l,$hval0_l)=each %wr_hash_l ) {
+    while ( ($hkey0_l,$hval0_l)=each %{$inv_hosts_href_l} ) {
 	#hkey0_l=inv-host
+	$wr_file_l=$dyn_fwrules_files_dir_l.'/'.$hkey0_l.'_recreate_ipsets.sh';
 	
-	@wr_arr_l=@{$hval0_l};
-	
-	if ( $#wr_arr_l!=-1 ) {
-	    $wr_file_l=$dyn_fwrules_files_dir_l.'/'.$hkey0_l.'_recreate_ipsets.sh';
-	    
+	if ( exists($wr_hash_l{$hkey0_l}) ) { # if exists content for 'recreate_ipsets.sh'
+	    @wr_arr_l=@{$wr_hash_l{$hkey0_l}};
+	        	
 	    # 1) form array of commands for remove ipset xml-s
-		#rm -rf  /etc/firewalld/ipsets/* +
-		#or "firewall-cmd --permanent --delete-ipset=some_ipset_name"
+	    	#rm -rf  /etc/firewalld/ipsets/* +
+	    	#or "firewall-cmd --permanent --delete-ipset=some_ipset_name"
 	    @wr_arr_l=(' ','rm -rf  /etc/firewalld/ipsets/*;','firewall-cmd --reload;',' ',@wr_arr_l);
 	    ###
-
+	    
 	    # 2) form array of commands for get and save ipset entries (for permanent ipsets) and save it to the '$remote_dir_for_absible_helper_l'
-		#"firewall-cmd --permanent --ipset=some_ipset_name --get-entries"
-		###
-		#%permanet_ipset_names_l=(); # permanent ipset names (not tmplt names) at each inv-host
-    		    #key=inv-host, value=array of permanent ipset names at current inv-host
+	    	#"firewall-cmd --permanent --ipset=some_ipset_name --get-entries"
+	    	###
+	    	#%permanet_ipset_names_l=(); # permanent ipset names (not tmplt names) at each inv-host
+    	    	    #key=inv-host, value=array of permanent ipset names at current inv-host
 	    if ( exists($permanet_ipset_names_l{$hkey0_l}) ) {
 		foreach $arr_el0_l ( @{$permanet_ipset_names_l{$hkey0_l}} ) {
 		    #arr_el0_l=permanent ipset name
 		    $ipset_list_l="$remote_dir_for_absible_helper_l/$arr_el0_l".'-list.txt';
 		    $wr_str_l="if [ -s \"/etc/firewalld/ipsets/$arr_el0_l.xml\" ]; then firewall-cmd --permanent --ipset=$arr_el0_l --get-entries > $ipset_list_l; fi;";
 		    @wr_arr_l=($wr_str_l,@wr_arr_l);
-		    
+		    	
 		    $wr_str_l=undef;
 		    $ipset_list_l=undef;
 		}
 	    }
 	    ###
-	    
+	    	
 	    # insert compiler path at the begin of the script
 	    @wr_arr_l=(
 		'#!/usr/bin/bash',
@@ -2541,7 +2543,7 @@ sub generate_shell_script_for_recreate_ipsets {
 		@wr_arr_l
 	    );
 	    ###
-
+	    
 	    # 3) form array of commands for add saved entries to permanent ipsets (for add to end of the wr_array_l)
 		#"firewall-cmd --permanent --ipset=some_ipset_name --add-entries-from-file=some_ipset_list.txt"
 	    if ( exists($permanet_ipset_names_l{$hkey0_l}) ) {
@@ -2550,24 +2552,25 @@ sub generate_shell_script_for_recreate_ipsets {
 		    $ipset_list_l="$remote_dir_for_absible_helper_l/$arr_el0_l".'-list.txt';
 		    $wr_str_l="if [ -s \"$ipset_list_l\" ]; then firewall-cmd --permanent --ipset=$arr_el0_l --add-entries-from-file=\"$ipset_list_l\"; rm -f $ipset_list_l; fi;";
 		    push(@wr_arr_l,$wr_str_l);
-		    
+		    	
 		    $wr_str_l=undef;
 		    $ipset_list_l=undef;
 		}
 	    }
 	    ###
-	    
+	    	
 	    @wr_arr_l=(@wr_arr_l,' ','firewall-cmd --reload;');
-	    
-	    $exec_res_l=&rewrite_file_from_array_ref($wr_file_l,\@wr_arr_l);
-	    #$file_l,$aref_l
-	    if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
-	    
-	    $wr_file_l=undef;
-	    @wr_arr_l=();
 	}
-	
-	@wr_arr_l=();	
+	else { # if not exists content for 'recreate_ipsets.sh'
+	    @wr_arr_l=('#!/usr/bin/bash',' ','#NO NEED TO RECREATE IPSETS');
+	}
+
+	$exec_res_l=&rewrite_file_from_array_ref($wr_file_l,\@wr_arr_l);
+	#$file_l,$aref_l
+	if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
+	    	
+	$wr_file_l=undef;
+	@wr_arr_l=();
     }
     
     ($hkey0_l,$hval0_l)=(undef,undef);
@@ -2580,6 +2583,9 @@ sub generate_shell_script_for_recreate_firewall_zones {
     my ($dyn_fwrules_files_dir_l,$input_hash4proc_href_l)=@_;
     #$dyn_fwrules_files_dir_l=$dyn_fwrules_files_dir_g
     #$input_hash4proc_href_l=hash-ref for %input_hash4proc_g (hash with hash refs for input)
+    
+    my $inv_hosts_href_l=${$input_hash4proc_href_l}{'inventory_hosts_href'};
+    #$inv_hosts_href_l=hash-ref for %invetory_hosts_g
     
     my $conf_firewalld_href_l=${$input_hash4proc_href_l}{'h00_conf_firewalld_href'};
     #$conf_firewalld_href_l=hash-ref for %h00_conf_firewalld_hash_g
@@ -3457,23 +3463,27 @@ sub generate_shell_script_for_recreate_firewall_zones {
 
     # create scripts for each host
 	#@begin_script_arr_l
-    while ( ($hkey0_l,$hval0_l)=each %wr_hash_l ) {
+    while ( ($hkey0_l,$hval0_l)=each %{$inv_hosts_href_l} ) {
 	#$hkey0_l=inv-host
 	    #subkeys: custom, standard, custom_remove, std_recreate
 	$wr_file_l=$dyn_fwrules_files_dir_l.'/'.$hkey0_l.'_recreate_fw_zones.sh';
-	@wr_arr_l=(@begin_script_arr_l);
-	if ( exists(${$hval0_l}{'std_recreate'}) ) { @wr_arr_l=(@wr_arr_l,@{${$hval0_l}{'std_recreate'}}); }
-	if ( exists(${$hval0_l}{'custom_remove'}) ) { @wr_arr_l=(@wr_arr_l,' ',@{${$hval0_l}{'custom_remove'}}); }
-	if ( exists(${$hval0_l}{'standard'}) ) { @wr_arr_l=(@wr_arr_l,' ',@{${$hval0_l}{'standard'}}); }
-	if ( exists(${$hval0_l}{'custom'}) ) { @wr_arr_l=(@wr_arr_l,@{${$hval0_l}{'custom'}}); }
-	@wr_arr_l=(@wr_arr_l,'firewall-cmd --reload;');
 	
-	$exec_res_l=&rewrite_file_from_array_ref($wr_file_l,\@wr_arr_l);
+	if ( exists($wr_hash_l{$hkey0_l}) ) {    
+	    @wr_arr_l=(@begin_script_arr_l);
+	    if ( exists($wr_hash_l{$hkey0_l}{'std_recreate'}) ) { @wr_arr_l=(@wr_arr_l,@{$wr_hash_l{$hkey0_l}{'std_recreate'}}); }
+	    if ( exists($wr_hash_l{$hkey0_l}{'custom_remove'}) ) { @wr_arr_l=(@wr_arr_l,' ',@{$wr_hash_l{$hkey0_l}{'custom_remove'}}); }
+	    if ( exists($wr_hash_l{$hkey0_l}{'standard'}) ) { @wr_arr_l=(@wr_arr_l,' ',@{$wr_hash_l{$hkey0_l}{'standard'}}); }
+	    if ( exists($wr_hash_l{$hkey0_l}{'custom'}) ) { @wr_arr_l=(@wr_arr_l,@{$wr_hash_l{$hkey0_l}{'custom'}}); }
+	    @wr_arr_l=(@wr_arr_l,'firewall-cmd --reload;');
+	}
+	else { @wr_arr_l=('#!/usr/bin/bash',' ','#NO NEED TO RECREATE FIREWALL ZONES'); }
+
+        $exec_res_l=&rewrite_file_from_array_ref($wr_file_l,\@wr_arr_l);
         #$file_l,$aref_l
         if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
-
-	$wr_file_l=undef;
-	@wr_arr_l=();
+	    
+        $wr_file_l=undef;
+        @wr_arr_l=();
     }
     
     ($hkey0_l,$hval0_l)=(undef,undef);
@@ -3487,6 +3497,9 @@ sub generate_shell_script_for_recreate_policies {
     #$dyn_fwrules_files_dir_l=$dyn_fwrules_files_dir_g
     #$input_hash4proc_href_l=hash-ref for %input_hash4proc_g (hash with hash refs for input)
     
+    my $inv_hosts_href_l=${$input_hash4proc_href_l}{'inventory_hosts_href'};
+    #$inv_hosts_href_l=hash-ref for %invetory_hosts_g
+
     my $ipset_templates_href_l=${$input_hash4proc_href_l}{'h01_conf_ipset_templates_href'};
     #$ipset_templates_href_l=hash-ref for %h01_conf_ipset_templates_hash_g
     	#$h01_conf_ipset_templates_hash_g{'temporary/permanent'}{ipset_template_name--TMPLT}->
@@ -3994,20 +4007,24 @@ sub generate_shell_script_for_recreate_policies {
 
     # create scripts for each host
 	#@begin_script_arr_l
-    while ( ($hkey0_l,$hval0_l)=each %wr_hash_l ) {
+    while ( ($hkey0_l,$hval0_l)=each %{$inv_hosts_href_l} ) {
 	#$hkey0_l=inv-host
 	    #subkeys: policies_remove, policies_recreate
 	$wr_file_l=$dyn_fwrules_files_dir_l.'/'.$hkey0_l.'_recreate_policies.sh';
-	@wr_arr_l=(@begin_script_arr_l);
-	if ( exists(${$hval0_l}{'policies_remove'}) ) { @wr_arr_l=(@wr_arr_l,@{${$hval0_l}{'policies_remove'}}); }
-	if ( exists(${$hval0_l}{'policies_recreate'}) ) { @wr_arr_l=(@wr_arr_l,' ',@{${$hval0_l}{'policies_recreate'}}); }
 	
-	@wr_arr_l=(@wr_arr_l,'firewall-cmd --reload');
+	if ( exists($wr_hash_l{$hkey0_l}) ) {    
+	    @wr_arr_l=(@begin_script_arr_l);
+	    if ( exists($wr_hash_l{$hkey0_l}{'policies_remove'}) ) { @wr_arr_l=(@wr_arr_l,@{$wr_hash_l{$hkey0_l}{'policies_remove'}}); }
+	    if ( exists($wr_hash_l{$hkey0_l}{'policies_recreate'}) ) { @wr_arr_l=(@wr_arr_l,' ',@{$wr_hash_l{$hkey0_l}{'policies_recreate'}}); }
+	    
+	    @wr_arr_l=(@wr_arr_l,'firewall-cmd --reload');
+	}
+	else { @wr_arr_l=('#!/usr/bin/bash',' ','#NO NEED TO RECREATE POLICIES'); }
 	
 	$exec_res_l=&rewrite_file_from_array_ref($wr_file_l,\@wr_arr_l);
-        #$file_l,$aref_l
-        if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
-
+    	#$file_l,$aref_l
+    	if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
+	    
 	$wr_file_l=undef;
 	@wr_arr_l=();
     }
