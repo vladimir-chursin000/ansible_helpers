f8197766aa428882963cf88e1802e5308626a7d7
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-03-17 21:59:34 +0400

    202303172158

--- conf_firewalld/rhel8_based/perl_subs/apply_IPSET_files_operation.pl
+++ conf_firewalld/rhel8_based/perl_subs/apply_IPSET_files_operation.pl
@@ -8,6 +8,8 @@ sub apply_IPSET_files_operation_main {
     
     my $return_str_l='OK';
     
+    ######
+
     &ops_with_local_dyn_ipsets_files_dir($dyn_ipsets_files_dir_l);
     #$dyn_ipsets_files_dir_l
     #$dyn_ipsets_files_dir_l/remove_queue
@@ -15,6 +17,8 @@ sub apply_IPSET_files_operation_main {
     #$dyn_ipsets_files_dir_l/add_queue
         # for copy content of dir '../add_queue/inv-host' to remote host to dir '~/ansible_helpers/conf_firewalld/ipset_files/add_queue
     ###
+    
+    ######
     
     &ops_with_local_ipset_input_dir($ipset_input_dir_l);
     #$ipset_input_dir_l
@@ -24,6 +28,8 @@ sub apply_IPSET_files_operation_main {
     #$ipset_input_dir_l/errors
     #$ipset_input_dir_l/history
     ###
+    
+    ######
     
     return $return_str_l;
 }
