2734f2277a7effe8e5ea55f9bb1dad8162e6fc6a
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-05-27 20:53:50 +0400

    202305272053

--- conf_firewalld/rhel8_based/perl_subs/generate_shell_for_ipsets.pl
+++ conf_firewalld/rhel8_based/perl_subs/generate_shell_for_ipsets.pl
@@ -156,7 +156,7 @@ sub generate_shell_script_for_recreate_ipsets {
     if ( $return_str_l!~/^OK$/ ) { return $return_str_l; }
     ###
 
-    # create scripts for copy to remote hosts
+    # create script "recreate_permanent_ipsets" for copy to remote hosts (BEGIN)
     while ( ($hkey0_l,$hval0_l)=each %{$inv_hosts_href_l} ) {
         #hkey0_l=inv-host
         $wr_file_l=$dyn_fwrules_files_dir_l.'/'.$hkey0_l.'_recreate_ipsets.sh';
@@ -189,20 +189,20 @@ sub generate_shell_script_for_recreate_ipsets {
                 }
             }
 	    
-	    #"ipset list some_ipset_name > some_ipset_name.txt"
-	    #readarray -t ARRAY < <(ipset list temporary_ipset | grep -i timeout | grep -v 'Header');
-		#echo -e "#!/usr/bin/bash\n" > temporary_ipset.sh; 
-		#for AE in "${ARRAY[@]}"; do echo "ipset add temporary_ipset $AE;" >> temporary_ipset.sh; done;
-		#echo -e "\nrm -f temporary_ipset.sh;" >> temporary_ipset.sh;
-	    ###
-	    #my %temporary_ipset_names_l=(); # temporary ipset names (not tmplt names) at each inv-host
-    		#key=inv-host, value=array of temporary ipset names at current inv-host
-            if ( exists($temporary_ipset_names_l{$hkey0_l}) ) { # for temporary ipsets
-		foreach $arr_el0_l ( @{$temporary_ipset_names_l{$hkey0_l}} ) {
-                    #arr_el0_l=temporary ipset name
-		    
-		}
-	    }
+	    #!!!#"ipset list some_ipset_name > some_ipset_name.txt"
+	    #!!!#readarray -t ARRAY < <(ipset list temporary_ipset | grep -i timeout | grep -v 'Header');
+		#!!!#echo -e "#!/usr/bin/bash\n" > temporary_ipset.sh; 
+		#!!!#for AE in "${ARRAY[@]}"; do echo "ipset add temporary_ipset $AE;" >> temporary_ipset.sh; done;
+		#!!!#echo -e "\nrm -f temporary_ipset.sh;" >> temporary_ipset.sh;
+	    #!!!###
+	    #!!!#my %temporary_ipset_names_l=(); # temporary ipset names (not tmplt names) at each inv-host
+    		#!!!#key=inv-host, value=array of temporary ipset names at current inv-host
+            #!!!if ( exists($temporary_ipset_names_l{$hkey0_l}) ) { # for temporary ipsets
+		#!!!foreach $arr_el0_l ( @{$temporary_ipset_names_l{$hkey0_l}} ) {
+                    #!!!#arr_el0_l=temporary ipset name
+		    #!!!
+		#!!!}
+	    #!!!}
             ### (3-end)
         
             # 4) form array of commands for add saved entries to permanent ipsets (for add to end of the wr_array_l)
@@ -235,17 +235,17 @@ sub generate_shell_script_for_recreate_ipsets {
             @wr_arr_l=(@wr_arr_l,' ','firewall-cmd --reload;');
 	    ###
 	    
-	    # 7) form array of commands for add saved entries to temporary ipsets (for add to end of the wr_array_l)
-	    #"ipset add some_ipset_name 11.1.1.2 timeout 500" where "11.1.1.2 timeout 500" is from some_ipset_name.txt
-            if ( exists($temporary_ipset_names_l{$hkey0_l}) ) { # for temporary ipsets
-		@wr_arr_l=(@wr_arr_l,' ');
-		
-		foreach $arr_el0_l ( @{$temporary_ipset_names_l{$hkey0_l}} ) {
-                    #arr_el0_l=temporary ipset name
-		    
-		}
-	    }
-	    ### (7-end)
+	    #!!!# 7) form array of commands for add saved entries to temporary ipsets (for add to end of the wr_array_l)
+	    #!!!#"ipset add some_ipset_name 11.1.1.2 timeout 500" where "11.1.1.2 timeout 500" is from some_ipset_name.txt
+            #!!!if ( exists($temporary_ipset_names_l{$hkey0_l}) ) { # for temporary ipsets
+		#!!!@wr_arr_l=(@wr_arr_l,' ');
+		#!!!
+		#!!!foreach $arr_el0_l ( @{$temporary_ipset_names_l{$hkey0_l}} ) {
+                    #!!!#arr_el0_l=temporary ipset name
+		    #!!!
+		#!!!}
+	    #!!!}
+	    #!!!### (7-end)
         }
         elsif ( !exists($wr_hash_l{$hkey0_l}) && ${$conf_firewalld_href_l}{$hkey0_l}{'if_no_ipsets_conf_action'}=~/^remove$/ ) {
             @wr_arr_l=(
@@ -279,7 +279,10 @@ sub generate_shell_script_for_recreate_ipsets {
     }
 
     ($hkey0_l,$hval0_l)=(undef,undef);
-    ###
+    # create script "recreate_permanent_ipsets" for copy to remote hosts (END)
+    
+    # create scripts for recreate temporary ipsets for copy to remote hosts (BEGIN)
+    # create scripts for recreate temporary ipsets for copy to remote hosts (END)
 
     return $return_str_l;
 }
