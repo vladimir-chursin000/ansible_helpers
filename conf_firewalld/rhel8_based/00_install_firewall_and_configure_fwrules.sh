d2f33db7610c0e86beb8682ddc87a150e50bdca5
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-05-28 18:44:10 +0400

    202305281844

--- conf_firewalld/rhel8_based/00_install_firewall_and_configure_fwrules.sh
+++ conf_firewalld/rhel8_based/00_install_firewall_and_configure_fwrules.sh
@@ -2,7 +2,7 @@
 
 SELF_DIR="$(dirname $(readlink -f $0))";
 INV_FILE="$SELF_DIR/conf_firewall_hosts";
-PLAYBOOK='full_install_firewall_and_configure_fwrules_pb.yml';
+PLAYBOOK='full_install_firewall_n_conf_fwrules_pb.yml';
 LOG_DIR="$SELF_DIR/run_history";
 PLAYBOOK_BEFORE='fwrules_backup_pb.yml';
 GEN_DYN_FWRULES_RUN='yes';
