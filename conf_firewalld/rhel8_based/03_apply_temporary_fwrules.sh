bc6e047b9107fe0ba91af11a2170f8db735665fc
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-06-04 20:10:18 +0400

    202306042010

--- conf_firewalld/rhel8_based/03_apply_temporary_fwrules.sh
+++ conf_firewalld/rhel8_based/03_apply_temporary_fwrules.sh
@@ -4,7 +4,7 @@ SELF_DIR="$(dirname $(readlink -f $0))";
 INV_FILE="$SELF_DIR/conf_firewall_hosts";
 PLAYBOOK='03_IMPORT_fwrules_apply_temporary_pb.yml';
 LOG_DIR="$SELF_DIR/run_history";
-PLAYBOOK_BEFORE='no';
+PLAYBOOK_BEFORE='02_fwrules_backup_pb.yml';
 GEN_DYN_FWRULES_RUN='yes_with_rollback';
 
 $SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR" "$PLAYBOOK_BEFORE" "$GEN_DYN_FWRULES_RUN";
