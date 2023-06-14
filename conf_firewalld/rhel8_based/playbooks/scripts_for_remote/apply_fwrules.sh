e53f1823dc091496b37744e109b4f339a843d02b
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-06-14 23:11:43 +0400

    202306142311

--- conf_firewalld/rhel8_based/playbooks/scripts_for_remote/apply_fwrules.sh
+++ conf_firewalld/rhel8_based/playbooks/scripts_for_remote/apply_fwrules.sh
@@ -58,50 +58,50 @@ rm -rf $APPLY_RUN_INFO_DIR_str/*;
 #if [[ "$ROLLBACK_FWRULES_NEED_RUN_str" == "yes" ]]; then
 #    mkdir -p "$BACKUP_FOR_ROLLBACK_DIR_str";
 #    cp -r /etc/firewalld/* "$BACKUP_FOR_ROLLBACK_DIR_str";
+
+    # 1a) Save content of temporary ipsets (if need) to 'fwrules_backup_now'. For rollback.
+    ###
 #fi;
 ###
 
-# 2) Save content of temporary ipsets (if need) to 'fwrules_backup_now'. For rollback.
-###
-
-# 3) Restart firewalld "systemctl restart firewalld" (if need).
+# 2) Restart firewalld "systemctl restart firewalld" (if need).
     # If changed: firewalld.conf
 #if [[ "$FWCONFIG_CHANGED_str" == "yes" ]]; then
 #    systemctl restart firewalld;
 #fi;
 ###
 
-# 4) Recreate permanent ipsets (if need).
+# 3) Recreate permanent ipsets (if need).
 #if [[ "$RECREATE_PERMANENT_IPSETS_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_permanent_ipsets.sh" &> "$SELF_DIR_str/recreate_permanent_ipsets-res.txt";
 #fi;
 ###
 
-# 5) Recreate temporary ipsets (if need).
+# 4) Recreate temporary ipsets (if need).
 #if [[ "$RECREATE_TEMPORARY_IPSETS_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_temporary_ipsets.sh" &> "$SELF_DIR_str/recreate_temporary_ipsets-res.txt";
 #fi;
 ###
 
-# 6) Recreate firewalld zones (if need).
+# 5) Recreate firewalld zones (if need).
 #if [[ "$RECREATE_FW_ZONES_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_fw_zones.sh" &> "$SELF_DIR_str/recreate_fw_zones-res.txt";
 #fi;
 ###
 
-# 7) Recreate firewalld policies (if need).
+# 6) Recreate firewalld policies (if need).
 #if [[ "$RECREATE_POLICIES_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_policies.sh" &> "$SELF_DIR_str/recreate_policies-res.txt";    
 #fi;
 ###
 
-# 8) Re-add. If need to re-add ipsets elements from ansible-host as source.
+# 7) Re-add. If need to re-add ipsets elements from ansible-host as source.
 #if [[ -s "$SELF_DIR_str/re_add_permanent_ipsets_content.sh" ]]; then
 #    "$SELF_DIR_str/re_add_permanent_ipsets_content.sh" &> "$SELF_DIR_str/re_add_permanent_ipsets_content-res.txt";
 #fi;
 ###
 
-# 9) Reload "firewall-cmd --reload" (if need).
+# 8) Reload "firewall-cmd --reload" (if need).
     # If changed: recreate_permanent_ipsets.sh, recreate_temporary_ipsets.sh, recreate_fw_zones.sh, recreate_policies.sh,
     # If executed: restore_permanent_ipsets_content.sh, modify_permanent_ipsets_content.sh
 #if [[ "$RELOAD_NEED_RUN_str" == "yes" ]]; then
@@ -109,15 +109,15 @@ rm -rf $APPLY_RUN_INFO_DIR_str/*;
 #fi;
 ###
 
-# 10) Re-add. If need to re-add ipsets elements from ansible-host as source.
+# 9) Re-add. If need to re-add ipsets elements from ansible-host as source.
 #if [[ -s "$SELF_DIR_str/re_add_temporary_ipsets_content.sh" ]]; then
 #    "$SELF_DIR_str/re_add_temporary_ipsets_content.sh" &> "$SELF_DIR_str/re_add_temporary_ipsets_content-res.txt";
 #fi;
 ###
 
-# 11) Rollback all changes (if need).
+# 10) Rollback all changes (if need).
     # For rollback -> saved permanent ipsets content from 'fwrules_backup_now' (at 1 step).
-    # For rollback -> saved temporary ipsets content from 'fwrules_backup_now' (at 2 step).
+    # For rollback -> saved temporary ipsets content from 'fwrules_backup_now' (at 1a step).
 #if [[ "$ROLLBACK_FWRULES_NEED_RUN_str" == "yes" ]]; then
 #    nohup sh -c '~/ansible_helpers/conf_firewalld/rollback_fwrules_changes.sh >/dev/null 2>&1' & sleep 1;
 #fi;
