7441acee3c2b3611b183adc89dfb358903f678ef
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-06-15 01:58:45 +0400

    202306150158

--- conf_firewalld/rhel8_based/playbooks/scripts_for_remote/apply_fwrules.sh
+++ conf_firewalld/rhel8_based/playbooks/scripts_for_remote/apply_fwrules.sh
@@ -21,10 +21,6 @@ RELOAD_NEED_RUN_str='no';
 ###VARS
 
 ###APPLY_RUN_INFO read
-if [[ -f "$APPLY_RUN_INFO_DIR_str/fwconfig_changed" ]]; then
-    FWCONFIG_CHANGED_str='yes';
-fi;
-
 if [[ -f "$APPLY_RUN_INFO_DIR_str/recreate_permanent_ipsets_changed" ]]; then
     RECREATE_PERMANENT_IPSETS_CHANGED_str='yes';
     RELOAD_NEED_RUN_str='yes';
@@ -49,6 +45,11 @@ if [[ -f "$APPLY_RUN_INFO_DIR_str/rollback_fwrules_need_run" ]]; then
     ROLLBACK_FWRULES_NEED_RUN_str='yes';
 fi;
 
+if [[ -f "$APPLY_RUN_INFO_DIR_str/fwconfig_changed" ]]; then
+    FWCONFIG_CHANGED_str='yes';
+    RELOAD_NEED_RUN_str='no';
+fi;
+
 rm -rf $APPLY_RUN_INFO_DIR_str/*;
 ###APPLY_RUN_INFO read
 
@@ -65,49 +66,49 @@ if [[ "$ROLLBACK_FWRULES_NEED_RUN_str" == "yes" ]]; then
 fi;
 ###
 
-# 2) Restart firewalld "systemctl restart firewalld" (if need).
-    # If changed: firewalld.conf
-if [[ "$FWCONFIG_CHANGED_str" == "yes" ]]; then
-    systemctl restart firewalld;
-fi;
-###
-
-# 3) Recreate permanent ipsets (if need).
+# 2) Recreate permanent ipsets (if need).
 #if [[ "$RECREATE_PERMANENT_IPSETS_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_permanent_ipsets.sh" &> "$SELF_DIR_str/recreate_permanent_ipsets-res.txt";
 #fi;
 ###
 
-# 4) Recreate temporary ipsets (if need).
+# 3) Recreate temporary ipsets (if need).
 #if [[ "$RECREATE_TEMPORARY_IPSETS_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_temporary_ipsets.sh" &> "$SELF_DIR_str/recreate_temporary_ipsets-res.txt";
 #fi;
 ###
 
-# 5) Recreate firewalld zones (if need).
+# 4) Recreate firewalld zones (if need).
 #if [[ "$RECREATE_FW_ZONES_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_fw_zones.sh" &> "$SELF_DIR_str/recreate_fw_zones-res.txt";
 #fi;
 ###
 
-# 6) Recreate firewalld policies (if need).
+# 5) Recreate firewalld policies (if need).
 #if [[ "$RECREATE_POLICIES_CHANGED_str" == "yes" ]]; then
 #    "$SELF_DIR_str/recreate_policies.sh" &> "$SELF_DIR_str/recreate_policies-res.txt";    
 #fi;
 ###
 
-# 7) Re-add. If need to re-add ipsets elements from ansible-host as source.
+# 6) Re-add. If need to re-add ipsets elements from ansible-host as source.
 #if [[ -s "$SELF_DIR_str/re_add_permanent_ipsets_content.sh" ]]; then
 #    "$SELF_DIR_str/re_add_permanent_ipsets_content.sh" &> "$SELF_DIR_str/re_add_permanent_ipsets_content-res.txt";
 #fi;
 ###
 
-# 8) Reload "firewall-cmd --reload" (if need).
-    # If changed: recreate_permanent_ipsets.sh, recreate_temporary_ipsets.sh, recreate_fw_zones.sh, recreate_policies.sh,
-    # If executed: restore_permanent_ipsets_content.sh, modify_permanent_ipsets_content.sh
-#if [[ "$RELOAD_NEED_RUN_str" == "yes" ]]; then
-#    firewall-cmd --reload;
-#fi;
+# 7) Reload "firewall-cmd --reload" (if need).
+    # If changed: recreate_permanent_ipsets.sh, recreate_temporary_ipsets.sh, recreate_fw_zones.sh, recreate_policies.sh
+    # If executed: re_add_permanent_ipsets_content.sh
+if [[ "$RELOAD_NEED_RUN_str" == "yes" ]]; then
+    firewall-cmd --reload;
+fi;
+###
+
+# 8) Restart firewalld "systemctl restart firewalld" (if need).
+    # If changed: firewalld.conf
+if [[ "$FWCONFIG_CHANGED_str" == "yes" ]]; then
+    systemctl restart firewalld;
+fi;
 ###
 
 # 9) Re-add. If need to re-add ipsets elements from ansible-host as source.
