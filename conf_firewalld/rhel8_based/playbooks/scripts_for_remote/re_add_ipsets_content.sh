1b0ad708624394c26564f3ef14a1e31edabd9b66
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-07-21 23:34:58 +0400

    202307212334

--- conf_firewalld/rhel8_based/playbooks/scripts_for_remote/re_add_ipsets_content.sh
+++ conf_firewalld/rhel8_based/playbooks/scripts_for_remote/re_add_ipsets_content.sh
@@ -6,12 +6,16 @@ SELF_DIR_str="$(dirname $(readlink -f $0))";
 
 ###VARS
 OPERATION_IPSET_TYPE_str=$1;
+CONTENT_DIR_str='no';
+PREV_CONTENT_DIR_str='no';
 ###VARS
 
 ###MAIN
 if [[ "$OPERATION_IPSET_TYPE_str" == "permanent" ]]; then
-    
+    CONTENT_DIR_str="$SELF_DIR_str/permanent_ipsets";
+    PREV_CONTENT_DIR_str="$SELF_DIR_str/prev_permanent_ipsets";
 elif [[ "$OPERATION_IPSET_TYPE_str" == "temporary" ]]; then
-    
+    CONTENT_DIR_str="$SELF_DIR_str/temporary_ipsets";
+    PREV_CONTENT_DIR_str="$SELF_DIR_str/prev_temporary_ipsets";
 fi;
 ###MAIN
