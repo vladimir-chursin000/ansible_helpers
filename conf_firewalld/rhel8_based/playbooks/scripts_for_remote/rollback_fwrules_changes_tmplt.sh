cb685b3eb554539f8d24eeeb1ce4762b25fb5033
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-08-12 03:23:27 +0400

    202308120323

--- conf_firewalld/rhel8_based/playbooks/scripts_for_remote/rollback_fwrules_changes_tmplt.sh
+++ conf_firewalld/rhel8_based/playbooks/scripts_for_remote/rollback_fwrules_changes_tmplt.sh
@@ -27,116 +27,117 @@ NOW_YYYYMMDDHHMISS_str='';
 declare -a TMP_arr;
 ###VARS
 
+touch "$SELF_DIR_str/rollback_fwrules_changes_is_run_now";
+
 while :
-do
-    touch "$SELF_DIR_str/rollback_fwrules_changes_is_run_now";
+do    
     sleep 60;
 
     let "TIMEOUT_num-=1";
     if [[ "$TIMEOUT_num" -le "0" ]]; then
-	###DO ROLLBACK of firewall rules changes
-	if [ -d "$BACKUP_FOR_ROLLBACK_DIR_str" ]; then
-	    
-	    # rollback for script 'recreate_permanent_ipsets.sh'
-	    if [[ -f "$SELF_DIR_str/recreate_permanent_ipsets.sh" ]]; then
-		rm -rf $SELF_DIR_str/recreate_permanent_ipsets.sh;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_recreate_permanent_ipsets.sh" ]]; then
-		mv "$SELF_DIR_str/prev_recreate_permanent_ipsets.sh" "$SELF_DIR_str/recreate_permanent_ipsets.sh";
-	    fi;
-	    ###
-	    
-	    # rollback for script 'recreate_temporary_ipsets.sh'
-	    if [[ -f "$SELF_DIR_str/recreate_temporary_ipsets.sh" ]]; then
-		rm -rf $SELF_DIR_str/recreate_temporary_ipsets.sh;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_recreate_temporary_ipsets.sh" ]]; then
-		mv "$SELF_DIR_str/prev_recreate_temporary_ipsets.sh" "$SELF_DIR_str/recreate_temporary_ipsets.sh";
-	    fi;
-	    ###
-	    
-	    # rollback for script 'recreate_fw_zones.sh'
-	    if [[ -f "$SELF_DIR_str/recreate_fw_zones.sh" ]]; then
-		rm -rf $SELF_DIR_str/recreate_fw_zones.sh;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_recreate_fw_zones.sh" ]]; then
-		mv "$SELF_DIR_str/prev_recreate_fw_zones.sh" "$SELF_DIR_str/recreate_fw_zones.sh";
-	    fi;
-	    ###
-	    
-	    # rollback for script 'recreate_policies.sh'
-	    if [[ -f "$SELF_DIR_str/recreate_policies.sh" ]]; then
-		rm -rf $SELF_DIR_str/recreate_policies.sh;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_recreate_policies.sh" ]]; then
-		mv "$SELF_DIR_str/prev_recreate_policies.sh" "$SELF_DIR_str/recreate_policies.sh";
-	    fi;
-	    ###
-	    
-	    # rollback for file 'permanent_ipsets_flag_file'
-	    if [[ -f "$SELF_DIR_str/permanent_ipsets_flag_file" ]]; then
-		rm -rf $SELF_DIR_str/permanent_ipsets_flag_file;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_permanent_ipsets_flag_file" ]]; then
-		mv "$SELF_DIR_str/prev_permanent_ipsets_flag_file"  "$SELF_DIR_str/permanent_ipsets_flag_file";
-	    fi;
-	    ###
-	    
-	    # rollback for file 'temporary_ipsets_flag_file'
-	    if [[ -f "$SELF_DIR_str/temporary_ipsets_flag_file" ]]; then
-		rm -rf $SELF_DIR_str/temporary_ipsets_flag_file;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_temporary_ipsets_flag_file" ]]; then
-		mv "$SELF_DIR_str/prev_temporary_ipsets_flag_file"  "$SELF_DIR_str/temporary_ipsets_flag_file";
-	    fi;
-	    ###
-	    
-	    # rollback for dir 'permanent_ipsets'
-	    if [[ -d "$SELF_DIR_str/permanent_ipsets" ]]; then
-		rm -rf $SELF_DIR_str/permanent_ipsets;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_permanent_ipsets" ]]; then
-		mv "$SELF_DIR_str/prev_permanent_ipsets"  "$SELF_DIR_str/permanent_ipsets";
-	    fi;
-	    ###
-
-	    # rollback for dir 'temporary_ipsets'
-	    if [[ -d "$SELF_DIR_str/temporary_ipsets" ]]; then
-		rm -rf $SELF_DIR_str/temporary_ipsets;
-	    fi;
-	    if [[ -f "$SELF_DIR_str/prev_temporary_ipsets" ]]; then
-		mv "$SELF_DIR_str/prev_temporary_ipsets"  "$SELF_DIR_str/temporary_ipsets";
-	    fi;
-	    ###
-	    
-	    rm -rf /etc/firewalld/*;
-	    
-	    cp -r $BACKUP_FOR_ROLLBACK_DIR_str/* /etc/firewalld;
-	    
-	    systemctl restart firewalld;
-	    
-	    # restore temporary ipsets content
-	    if [[ -s "$BACKUP_FOR_ROLLBACK_DIR_str/temporary_ipsets_list.txt" ]]; then
-		while read -r LINE0_str; # LINE0_str = ipset_name
-		do
-		    # read ipset content from file LINE0_str
-		    if [[ -s "$TEMP_IPSET_CONT_BACKUP_FOR_ROLLBACK_DIR_str/$LINE0_str.txt" ]]; then
-			while read -r LINE1_str; # LINE1_str = one line with ipset entry
-			do
-			    TMP_arr=($LINE1_str); # 0=ip, 1=string "timeout", 2=timeout (num)
-			    ipset add $LINE0_str ${TMP_arr[0]} timeout ${TMP_arr[2]}; # restore ipset entry for ipset_name=LINE0_str
-			done < "$TEMP_IPSET_CONT_BACKUP_FOR_ROLLBACK_DIR_str/$LINE0_str.txt";
-		    fi;
-		    ###
-		done < "$BACKUP_FOR_ROLLBACK_DIR_str/temporary_ipsets_list.txt";
-	    fi;
-	    ###
-	    
-	    rm -rf $BACKUP_FOR_ROLLBACK_DIR_str/*; # remove backup files
-	fi;
-	###DO ROLLBACK of firewall rules changes
-	
-	rm -rf "$SELF_DIR_str/rollback_fwrules_changes_is_run_now";
-	exit;
+    	###DO ROLLBACK of firewall rules changes
+    	if [ -d "$BACKUP_FOR_ROLLBACK_DIR_str" ]; then
+    	    
+    	    # rollback for script 'recreate_permanent_ipsets.sh'
+    	    if [[ -f "$SELF_DIR_str/recreate_permanent_ipsets.sh" ]]; then
+    		rm -rf $SELF_DIR_str/recreate_permanent_ipsets.sh;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_recreate_permanent_ipsets.sh" ]]; then
+    		mv "$SELF_DIR_str/prev_recreate_permanent_ipsets.sh" "$SELF_DIR_str/recreate_permanent_ipsets.sh";
+    	    fi;
+    	    ###
+    	    
+    	    # rollback for script 'recreate_temporary_ipsets.sh'
+    	    if [[ -f "$SELF_DIR_str/recreate_temporary_ipsets.sh" ]]; then
+    		rm -rf $SELF_DIR_str/recreate_temporary_ipsets.sh;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_recreate_temporary_ipsets.sh" ]]; then
+    		mv "$SELF_DIR_str/prev_recreate_temporary_ipsets.sh" "$SELF_DIR_str/recreate_temporary_ipsets.sh";
+    	    fi;
+    	    ###
+    	    
+    	    # rollback for script 'recreate_fw_zones.sh'
+    	    if [[ -f "$SELF_DIR_str/recreate_fw_zones.sh" ]]; then
+    		rm -rf $SELF_DIR_str/recreate_fw_zones.sh;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_recreate_fw_zones.sh" ]]; then
+    		mv "$SELF_DIR_str/prev_recreate_fw_zones.sh" "$SELF_DIR_str/recreate_fw_zones.sh";
+    	    fi;
+    	    ###
+    	    
+    	    # rollback for script 'recreate_policies.sh'
+    	    if [[ -f "$SELF_DIR_str/recreate_policies.sh" ]]; then
+    		rm -rf $SELF_DIR_str/recreate_policies.sh;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_recreate_policies.sh" ]]; then
+    		mv "$SELF_DIR_str/prev_recreate_policies.sh" "$SELF_DIR_str/recreate_policies.sh";
+    	    fi;
+    	    ###
+    	    
+    	    # rollback for file 'permanent_ipsets_flag_file'
+    	    if [[ -f "$SELF_DIR_str/permanent_ipsets_flag_file" ]]; then
+    		rm -rf $SELF_DIR_str/permanent_ipsets_flag_file;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_permanent_ipsets_flag_file" ]]; then
+    		mv "$SELF_DIR_str/prev_permanent_ipsets_flag_file"  "$SELF_DIR_str/permanent_ipsets_flag_file";
+    	    fi;
+    	    ###
+    	    
+    	    # rollback for file 'temporary_ipsets_flag_file'
+    	    if [[ -f "$SELF_DIR_str/temporary_ipsets_flag_file" ]]; then
+    		rm -rf $SELF_DIR_str/temporary_ipsets_flag_file;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_temporary_ipsets_flag_file" ]]; then
+    		mv "$SELF_DIR_str/prev_temporary_ipsets_flag_file"  "$SELF_DIR_str/temporary_ipsets_flag_file";
+    	    fi;
+    	    ###
+    	    
+    	    # rollback for dir 'permanent_ipsets'
+    	    if [[ -d "$SELF_DIR_str/permanent_ipsets" ]]; then
+    		rm -rf $SELF_DIR_str/permanent_ipsets;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_permanent_ipsets" ]]; then
+    		mv "$SELF_DIR_str/prev_permanent_ipsets"  "$SELF_DIR_str/permanent_ipsets";
+    	    fi;
+    	    ###
+    	
+    	    # rollback for dir 'temporary_ipsets'
+    	    if [[ -d "$SELF_DIR_str/temporary_ipsets" ]]; then
+    		rm -rf $SELF_DIR_str/temporary_ipsets;
+    	    fi;
+    	    if [[ -f "$SELF_DIR_str/prev_temporary_ipsets" ]]; then
+    		mv "$SELF_DIR_str/prev_temporary_ipsets"  "$SELF_DIR_str/temporary_ipsets";
+    	    fi;
+    	    ###
+    	    
+    	    rm -rf /etc/firewalld/*;
+    	    
+    	    cp -r $BACKUP_FOR_ROLLBACK_DIR_str/* /etc/firewalld;
+    	    
+    	    systemctl restart firewalld;
+    	    
+    	    # restore temporary ipsets content
+    	    if [[ -s "$BACKUP_FOR_ROLLBACK_DIR_str/temporary_ipsets_list.txt" ]]; then
+    		while read -r LINE0_str; # LINE0_str = ipset_name
+    		do
+    		    # read ipset content from file LINE0_str
+    		    if [[ -s "$TEMP_IPSET_CONT_BACKUP_FOR_ROLLBACK_DIR_str/$LINE0_str.txt" ]]; then
+    			while read -r LINE1_str; # LINE1_str = one line with ipset entry
+    			do
+    			    TMP_arr=($LINE1_str); # 0=ip, 1=string "timeout", 2=timeout (num)
+    			    ipset add $LINE0_str ${TMP_arr[0]} timeout ${TMP_arr[2]}; # restore ipset entry for ipset_name=LINE0_str
+    			done < "$TEMP_IPSET_CONT_BACKUP_FOR_ROLLBACK_DIR_str/$LINE0_str.txt";
+    		    fi;
+    		    ###
+    		done < "$BACKUP_FOR_ROLLBACK_DIR_str/temporary_ipsets_list.txt";
+    	    fi;
+    	    ###
+    	    
+    	    rm -rf $BACKUP_FOR_ROLLBACK_DIR_str/*; # remove backup files
+    	fi;
+    	###DO ROLLBACK of firewall rules changes
+    	
+    	rm -rf "$SELF_DIR_str/rollback_fwrules_changes_is_run_now";
+    	exit;
     fi;
 done;
