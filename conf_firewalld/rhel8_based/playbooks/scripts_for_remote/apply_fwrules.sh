673cb3480c1199ddc2b0d237a7c00ca648c58af3
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-09-10 16:43:49 +0400

    202309101643

--- conf_firewalld/rhel8_based/playbooks/scripts_for_remote/apply_fwrules.sh
+++ conf_firewalld/rhel8_based/playbooks/scripts_for_remote/apply_fwrules.sh
@@ -36,6 +36,16 @@ NOW_YYYYMMDDHHMISS_str='';
 declare -a TMP_arr;
 ###VARS
 
+######FUNCTIONS
+function write_log_func() {
+    local local_log_str=$1;
+    local local_log_file_str=$2;
+    local local_now_yyyymmddhhmiss_str=`date '+%Y%m%d%H%M%S'`;
+
+    echo "$local_now_yyyymmddhhmiss_str;+$local_log_str" &>> $local_log_file_str;
+}
+######FUNCTIONS
+
 ###APPLY_RUN_INFO read
 NOW_YYYYMMDDHHMISS_str=`date '+%Y%m%d%H%M%S'`;
 
