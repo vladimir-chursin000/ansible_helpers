5ee891159a9e89920b45cf3932f36f675bc25bfc
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-09-14 03:06:29 +0400

    202309140306

--- conf_firewalld/rhel8_based/05_del_ipset_entry.sh
+++ conf_firewalld/rhel8_based/05_del_ipset_entry.sh
@@ -3,10 +3,10 @@
 # Do not change this variables
 SELF_DIR_str="$(dirname $(readlink -f $0))";
 INV_LIMIT_str='no';
-PLAYBOOK_str='';
+PLAYBOOK_str='03_IMPORT_fwrules_apply_immediately_pb.yml'; # def
 LOG_DIR_str="$SELF_DIR_str/run_history";
 PLAYBOOK_BEFORE_str='no'; #for run before script 'generate_dynamic_fwrules.pl' and/or PLAYBOOK
-GEN_DYN_FWRULES_RUN_str='no';
+GEN_DYN_FWRULES_RUN_str='yes'; # def
 
 ###VARS
 TMP_VAR_str='';
@@ -20,5 +20,8 @@ do
     fi;
 done;
 ###READ ARGV array
+
+###READ USER MANUAL INPUT (if the basic parameters are not filled in)
+###READ USER MANUAL INPUT
 
 $SELF_DIR_str/main.sh "$INV_LIMIT_str" "$PLAYBOOK_str" "$LOG_DIR_str" "$PLAYBOOK_BEFORE_str" "$GEN_DYN_FWRULES_RUN_str";
