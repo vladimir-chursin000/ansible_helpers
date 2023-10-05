6327e2b9296adfe62cda8ed158209e4645af907a
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-10-05 21:53:22 +0400

    202310052153

---
+++ conf_firewalld/rhel8_based/07_enable_crontab_ops_at_remote.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+###DEFAULT INPUT VARS (for main.sh)
+# Do not change this variables manually
+SELF_DIR_str="$(dirname $(readlink -f $0))";
+INV_LIMIT_str='no';
+PLAYBOOK_str='';
+LOG_DIR_str="$SELF_DIR_str/run_history";
+PLAYBOOK_BEFORE_str='no'; #for run before script 'generate_dynamic_fwrules.pl' and/or PLAYBOOK
+GEN_DYN_FWRULES_RUN_str='no';
+###DEFAULT INPUT VARS
+
+###VARS
+TMP_VAR_str='';
+###VARS
+
+###READ ARGV array
+for TMP_VAR_str in "$@"
+do
+    if [[ "$TMP_VAR_str" =~ ^"limit=" ]]; then
+        # possible argv values: host_name, "host_name1,host_name2,..." (host names from inventory = "conf_firewall_hosts"),
+            # group name from "00_conf_divisions_for_inv_hosts" (for example, gr_some_group).
+        INV_LIMIT_str=$(echo $TMP_VAR_str | sed s/^"limit="//);
+    fi;
+done;
+###READ ARGV array
+
+$SELF_DIR_str/main.sh "$INV_LIMIT_str" "$PLAYBOOK_str" "$LOG_DIR_str" "$PLAYBOOK_BEFORE_str" "$GEN_DYN_FWRULES_RUN_str";
