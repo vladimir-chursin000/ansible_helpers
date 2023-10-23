9cdd1ee03a99f49d86bd9825adc783d51d996e83
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-10-24 00:29:32 +0400

    202310240029

---
+++ SIMPLE_custom_ansible/04_scripts_for_run_playbooks/example_bash_script1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+SELF_DIR_str="$(dirname $(readlink -f $0))";
+
+INVENTORY_DIR_str="$SELF_DIR_str/..";
+
+INVENTORY_FILE_str="$INVENTORY_DIR_str/inventory";
+
+ansible-playbook -i $INVENTORY_FILE_str -u root --private-key=~/.ssh/id_rsa "$INVENTORY_DIR_str/02_playbooks/example_playbook1.yml" --extra-vars "@$SELF_DIR_str/example_bash_script1-vars.json";
