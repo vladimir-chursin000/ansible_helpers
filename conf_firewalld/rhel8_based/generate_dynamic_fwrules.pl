380421f4f896afcf67a8c45ff51c6e7490be1e73
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2023-11-21 01:16:09 +0400

    202311210116

--- conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
+++ conf_firewalld/rhel8_based/generate_dynamic_fwrules.pl
@@ -643,6 +643,10 @@ while ( 1 ) { # ONE RUN CYCLE begin
 	'h05_conf_rich_rules_sets_href'=>\%h05_conf_rich_rules_sets_hash_g,
 	'h66_conf_ipsets_FIN_href'=>\%h66_conf_ipsets_FIN_hash_g,
     );
+    #new
+    #$exec_res_g=&read_77_conf_zones_FIN($f77_conf_zones_FIN_path_g,\%input_hash4proc_g,\%h77_conf_zones_FIN_hash_g);
+    #$file_l,$input_hash4proc_href_l,$res_href_l
+
     $exec_res_g=&read_77_conf_zones_FIN($f77_conf_zones_FIN_path_g,\%input_hash4proc_g,\%h77_conf_zones_FIN_hash_g);
     #$file_l,$input_hash4proc_href_l,$res_href_l
     if ( $exec_res_g=~/^fail/ ) {
