5262526316224f2dceb187aea6a5a1a67a213da4
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-01-06 01:59:45 +0400

    202401060159

--- conf_firewalld/rhel8_based/perl_subs/read_02_2_conf_fwrules.pl
+++ conf_firewalld/rhel8_based/perl_subs/read_02_2_conf_fwrules.pl
@@ -46,24 +46,24 @@ sub read_02_2_conf_allowed_ports_sets {
     # check rules and save res to '%res_tmp_lv1_l' (begin)
     while ( ($hkey0_l,$hval0_l)=each %res_tmp_lv0_l ) { # cycle 0
         #hkey0_l=set_name, hval0_l=hash ref where key=string with rule params
-	
+    	
         delete($res_tmp_lv0_l{$hkey0_l}{'seq'}); # seq-array don't need here
-	
+    	
         # block for checks of strings with rule params (begin)   
         while ( ($hkey1_l,$hval1_l)=each %{$hval0_l} ) {
             #hkey1_l=string with rule params
-	    #string with rule params="host=param1,param2,etc"
-	    
-	    @tmp_arr0_l=split(/\=/,$hkey1_l);
-	    # 0 - host-id (all/group/list_of_hosts/single_host), 1 - str with params
-	    
-	    @params_arr_l=split(/\,/,$tmp_arr0_l[1]);
-	    
+    	    #string with rule params="host=param1,param2,etc"
+    	    
+    	    @tmp_arr0_l=split(/\=/,$hkey1_l);
+    	    # 0 - host-id (all/group/list_of_hosts/single_host), 1 - str with params
+    	    
+    	    @params_arr_l=split(/\,/,$tmp_arr0_l[1]);
+    	    
             if ( $#tmp_arr0_l!=1 ) {
                 $return_str_l="fail [$proc_name_l]. String with rule params ('$hkey1_l') is incorrect. It should be like 'host=params'";
                 last;
             }
-	    
+    	    
             ###
             $exec_res_l=&check_inv_host_by_type($tmp_arr0_l[0],$inv_hosts_href_l,$divisions_for_inv_hosts_href_l);
             #$inv_host_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l
@@ -73,61 +73,61 @@ sub read_02_2_conf_allowed_ports_sets {
             }
             $exec_res_l=undef;
             ###
-	    
-	    if ( $tmp_arr0_l[1]=~/^empty\,|\,empty\,|\,empty$/ ) {
+    	    
+    	    if ( $tmp_arr0_l[1]=~/^empty\,|\,empty\,|\,empty$/ ) {
                 $return_str_l="fail [$proc_name_l]. Special value 'empty' cannot be part of a list (rule param '$hkey1_l')";
                 last;
             }
-	    	    
-	    ###
-	    if ( $tmp_arr0_l[1]!~/^empty$/ ) {
-		foreach $arr_el0_l ( @params_arr_l ) {
-	    	    #$arr_el0_l=port
-	    	    
-	    	    $exec_res_l=&check_port_for_apply_to_fw_conf($arr_el0_l);
+    	    	    
+    	    ###
+    	    if ( $tmp_arr0_l[1]!~/^empty$/ ) {
+    		foreach $arr_el0_l ( @params_arr_l ) {
+    	    	    #$arr_el0_l=port
+    	    	    
+    	    	    $exec_res_l=&check_port_for_apply_to_fw_conf($arr_el0_l);
     	    	    #$port_str_l
     	    	    if ( $exec_res_l=~/^fail/ ) {
     	    		$return_str_l="fail [$proc_name_l] -> ".$exec_res_l;
             		last;
     	    	    }
-	    	    $exec_res_l=undef;
-		}
-		
-		if ( $return_str_l!~/^OK$/ ) { last; }
-	    }
-	    ###
-	    
+    	    	    $exec_res_l=undef;
+    		}
+    		
+    		if ( $return_str_l!~/^OK$/ ) { last; }
+    	    }
+    	    ###
+    	    
             # clear vars
             @tmp_arr0_l=();
-	    @params_arr_l=();
+    	    @params_arr_l=();
             ###
         }
-	
+    	
         if ( $return_str_l!~/^OK$/ ) { last; }
         # block for checks of strings with rule params (end)
-	
+    	
         # block for 'single_host' (prio >= 2/high) (begin)
         while ( ($hkey1_l,$hval1_l)=each %{$hval0_l} ) {
             #hkey1_l=string with rule params
             #string with rule params="host=param1,param2,etc"
-	    
+    	    
             @tmp_arr0_l=split(/\=/,$hkey1_l);
             # 0 - host-id (all/group/list_of_hosts/single_host), 1 - str with params
-	    
+    	    
             if ( $tmp_arr0_l[0]=~/^\S+$/ && $tmp_arr0_l[0]!~/\,|^all|^gr_/ ) {
-		@params_arr_l=split(/\,/,$tmp_arr0_l[1]);
-		
+    		@params_arr_l=split(/\,/,$tmp_arr0_l[1]);
+    		
                 foreach $arr_el0_l ( @params_arr_l ) {
                     if ( !exists($res_tmp_lv1_l{$hkey0_l}{$tmp_arr0_l[0]}{$arr_el0_l}) ) {
                         $res_tmp_lv1_l{$hkey0_l}{$tmp_arr0_l[0]}{$arr_el0_l}=1;
                         push(@{$res_tmp_lv1_l{$hkey0_l}{$tmp_arr0_l[0]}{'seq'}},$arr_el0_l);
                     }
                 }
-		
-		delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
-		
+    		
+    		delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
+    		
                 # clear vars
-		$arr_el0_l=undef;
+    		$arr_el0_l=undef;
                 @params_arr_l=();
                 ###
             }
@@ -137,19 +137,19 @@ sub read_02_2_conf_allowed_ports_sets {
             ###
         }
         # block for 'single_host' (end)
-	
+    	
         # block for 'host_list' (prio = 2) (begin)
         while ( ($hkey1_l,$hval1_l)=each %{$hval0_l} ) {
             #hkey1_l=string with rule params
             #string with rule params="host=param1,param2,etc"
-	    
+    	    
             @tmp_arr0_l=split(/\=/,$hkey1_l);
             # 0 - host-id (all/group/list_of_hosts/single_host), 1 - str with params
-	    
+    	    
             if ( $tmp_arr0_l[0]=~/^\S+\,\S+/ ) {
                 @host_list_l=split(/\,/,$tmp_arr0_l[0]);
                 @params_arr_l=split(/\,/,$tmp_arr0_l[1]);
-	    
+    	    
                 foreach $arr_el0_l ( @host_list_l ) {
                     #$arr_el0_l=inv-host
                     if ( !exists($res_tmp_lv1_l{$hkey0_l}{$arr_el0_l}) ) {
@@ -162,11 +162,11 @@ sub read_02_2_conf_allowed_ports_sets {
                         }
                     }
                 }
-	    	
-	    	delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
-	    
+    	    	
+    	    	delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
+    	    
                 # clear vars
-	    	($arr_el0_l,$arr_el1_l)=(undef,undef);
+    	    	($arr_el0_l,$arr_el1_l)=(undef,undef);
                 @host_list_l=();
                 @params_arr_l=();
                 ###
@@ -177,21 +177,21 @@ sub read_02_2_conf_allowed_ports_sets {
             ###
         }
         # block for 'host_list' (end)
-	
+    	
         # block for 'groups' (prio = 1) (begin)
         while ( ($hkey1_l,$hval1_l)=each %{$hval0_l} ) {
             #hkey1_l=string with rule params
             #string with rule params="host=param1,param2,etc"
-	    
+    	    
             @tmp_arr0_l=split(/\=/,$hkey1_l);
             # 0 - host-id (all/group/list_of_hosts/single_host), 1 - str with params
-	    
+    	    
             if ( $tmp_arr0_l[0]=~/^gr_\S+$/ ) {
                 @params_arr_l=split(/\,/,$tmp_arr0_l[1]);
-	    	
+    	    	
                 while ( ($hkey2_l,$hval2_l)=each %{${$divisions_for_inv_hosts_href_l}{$tmp_arr0_l[0]}} ) {
                     #$hkey2_l=inv-host from '00_conf_divisions_for_inv_hosts' by group name
-	    	    
+    	    	    
                     if ( !exists($res_tmp_lv1_l{$hkey0_l}{$hkey2_l}) ) {
                         foreach $arr_el0_l ( @params_arr_l ) {
                             if ( !exists($res_tmp_lv1_l{$hkey0_l}{$hkey2_l}{$arr_el0_l}) ) {
@@ -202,36 +202,36 @@ sub read_02_2_conf_allowed_ports_sets {
                         }
                     }
                 }
-	    	
-		delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
-	    
+    	    	
+    		delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
+    	    
                 # clear vars
                 $arr_el0_l=undef;
                 ($hkey2_l,$hval2_l)=(undef,undef);
                 @params_arr_l=();
                 ###
             }
-	    
+    	    
             # clear vars
             @tmp_arr0_l=();
             ###
         }
         # block for 'groups' (end)
-	
+    	
         # block for 'all' (prio = 0/min) (begin)
         while ( ($hkey1_l,$hval1_l)=each %{$hval0_l} ) {
             #hkey1_l=string with rule params
             #string with rule params="host=param1,param2,etc"
-	    
+    	    
             @tmp_arr0_l=split(/\=/,$hkey1_l);
             # 0 - host-id (all/group/list_of_hosts/single_host), 1 - str with params
-	    
+    	    
             if ( $tmp_arr0_l[0]=~/^all$/ ) {
                 @params_arr_l=split(/\,/,$tmp_arr0_l[1]);
-	    	
+    	    	
                 while ( ($hkey2_l,$hval2_l)=each %{$inv_hosts_href_l} ) {
                     #$hkey2_l=inv-host from '%inventory_hosts_g'
-	    	    
+    	    	    
                     if ( !exists($res_tmp_lv1_l{$hkey0_l}{$hkey2_l}) ) {
                         foreach $arr_el0_l ( @params_arr_l ) {
                             if ( !exists($res_tmp_lv1_l{$hkey0_l}{$hkey2_l}{$arr_el0_l}) ) {
@@ -242,22 +242,22 @@ sub read_02_2_conf_allowed_ports_sets {
                         }
                     }
                 }
-	    	
-	    	delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
-	    	
+    	    	
+    	    	delete($res_tmp_lv0_l{$hkey0_l}{$hkey1_l});
+    	    	
                 # clear vars
                 $arr_el0_l=undef;
                 ($hkey2_l,$hval2_l)=(undef,undef);
                 @params_arr_l=();
                 ###
             }
-	    
+    	    
             # clear vars
             @tmp_arr0_l=();
             ###
         }
         # block for 'all' (end)
-	
+    	
         # clear vars
         $exec_res_l=undef;
         ($hkey1_l,$hval1_l)=(undef,undef);
