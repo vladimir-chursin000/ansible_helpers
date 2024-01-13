d891911db5dc5568216f6e64c5cd68e7077a44d7
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-01-14 01:26:35 +0400

    202401140126

--- conf_firewalld/rhel8_based/perl_subs/read_88_conf_fwrules.pl
+++ conf_firewalld/rhel8_based/perl_subs/read_88_conf_fwrules.pl
@@ -272,9 +272,9 @@ sub read_88_conf_policies_FIN_v2 {
                     @arr1_l=@{${$allowed_services_sets_href_l}{$set_name_l}{$inv_host_l}{'seq'}};
         	
                     if ( $arr1_l[0]!~/^empty$/ ) {
-                        $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_services_set'}=[@arr1_l];
+                        $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_services_set'}=[@arr1_l];
                     }
-                    else { $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_services_set_empty'}=1; }
+                    else { $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_services_set_empty'}=1; }
         	
                     # clear vars
                     @arr1_l=();
@@ -309,9 +309,9 @@ sub read_88_conf_policies_FIN_v2 {
                     @arr1_l=@{${$allowed_ports_sets_href_l}{$set_name_l}{$inv_host_l}{'seq'}};
         	
                     if ( $arr1_l[0]!~/^empty$/ ) {
-                        $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_ports_set'}=[@arr1_l];
+                        $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_ports_set'}=[@arr1_l];
                     }
-                    else { $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_ports_set_empty'}=1; }
+                    else { $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_ports_set_empty'}=1; }
         	
                     # clear vars
                     @arr1_l=();
@@ -346,9 +346,9 @@ sub read_88_conf_policies_FIN_v2 {
                     @arr1_l=@{${$allowed_ports_sets_href_l}{$set_name_l}{$inv_host_l}{'seq'}};
         	
                     if ( $arr1_l[0]!~/^empty$/ ) {
-                        $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_source_ports_set'}=[@arr1_l];
+                        $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_source_ports_set'}=[@arr1_l];
                     }
-                    else { $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_source_ports_set_empty'}=1; }
+                    else { $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_source_ports_set_empty'}=1; }
         	
                     # clear vars
                     @arr1_l=();
@@ -383,9 +383,9 @@ sub read_88_conf_policies_FIN_v2 {
                     @arr1_l=@{${$allowed_protocols_sets_href_l}{$set_name_l}{$inv_host_l}{'seq'}};
         	
                     if ( $arr1_l[0]!~/^empty$/ ) {
-                        $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_protocols_set'}=[@arr1_l];
+                        $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_protocols_set'}=[@arr1_l];
                     }
-                    else { $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'allowed_protocols_set_empty'}=1; }
+                    else { $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'allowed_protocols_set_empty'}=1; }
         	
                     # clear vars
                     @arr1_l=();
@@ -420,9 +420,9 @@ sub read_88_conf_policies_FIN_v2 {
                     @arr1_l=@{${$icmp_blocks_sets_href_l}{$set_name_l}{$inv_host_l}{'seq'}};
         	
                     if ( $arr1_l[0]!~/^empty$/ ) {
-                        $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'icmp_blocks_set'}=[@arr1_l];
+                        $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'icmp_blocks_set'}=[@arr1_l];
                     }
-                    else { $res_tmp_lv1_l{$zone_type_l}{$inv_host_l}{${$hval0_l}[0]}{'icmp_blocks_set_empty'}=1; }
+                    else { $res_tmp_lv1_l{$inv_host_l}{${$hval0_l}[0]}{'icmp_blocks_set_empty'}=1; }
         	
                     # clear vars
                     @arr1_l=();
