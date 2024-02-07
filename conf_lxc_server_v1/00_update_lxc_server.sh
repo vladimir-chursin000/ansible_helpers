aaed998ab85b07e65d5ea591c09843b6020ce23d
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-02-08 00:45:59 +0400

    202402080045

--- conf_lxc_server_v1/00_update_lxc_server.sh
+++ conf_lxc_server_v1/00_update_lxc_server.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/bash
 
+######DEFAULT INPUT VARS (for main.sh)
+# Do not change this variables manually
 SELF_DIR="$(dirname $(readlink -f $0))";
 INV_FILE="$SELF_DIR/lxc_server_hosts";
 PLAYBOOK='update_lxc_serv_playbook.yml';
 LOG_DIR="$SELF_DIR/run_history";
+######DEFAULT INPUT VARS
+
+######VARS
+TMP_VAR_str='';
+######VARS
 
 $SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR";
