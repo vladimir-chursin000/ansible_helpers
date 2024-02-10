11411d6a9c93f5567d2b8963a5cbfd229e8f7362
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-02-10 09:23:07 +0400

    202402100923

--- conf_lxc_server_v1/00_install_lxc_server.sh
+++ conf_lxc_server_v1/00_install_lxc_server.sh
@@ -2,14 +2,25 @@
 
 ######DEFAULT INPUT VARS (for main.sh)
 # Do not change this variables manually
-SELF_DIR="$(dirname $(readlink -f $0))";
-INV_FILE="$SELF_DIR/lxc_server_hosts";
-PLAYBOOK='full_install_lxc_serv_playbook.yml';
-LOG_DIR="$SELF_DIR/run_history";
+SELF_DIR_str="$(dirname $(readlink -f $0))";
+INV_LIMIT_str="$SELF_DIR_str/lxc_server_hosts";
+PLAYBOOK_str='full_install_lxc_serv_playbook.yml';
+LOG_DIR_str="$SELF_DIR_str/run_history";
 ######DEFAULT INPUT VARS
 
 ######VARS
 TMP_VAR_str='';
 ######VARS
 
-$SELF_DIR/main.sh "$INV_FILE" "$PLAYBOOK" "$LOG_DIR";
+######READ ARGV array
+for TMP_VAR_str in "$@"
+do
+    if [[ "$TMP_VAR_str" =~ ^"limit=" ]]; then
+        # possible argv values: host_name, "host_name1,host_name2,..." (host names from inventory = "lxc_server_hosts"),
+            # group name from "00_conf_divisions_for_inv_hosts" (for example, gr_some_group).
+        INV_LIMIT_str=$(echo $TMP_VAR_str | sed s/^"limit="//);
+    fi;
+done;
+######READ ARGV array
+
+$SELF_DIR_str/main.sh "$INV_LIMIT_str" "$PLAYBOOK_str" "$LOG_DIR_str";
--- conf_lxc_server_v1/main.sh
+++ conf_lxc_server_v1/main.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/bash
 
-SELF_DIR="$(dirname $(readlink -f $0))";
+SELF_DIR_str="$(dirname $(readlink -f $0))";
 
 ###ARGV
-INV_FILE=$1;
-PLAYBOOK=$2;
-LOG_DIR=$3;
+INV_LIMIT_str=$1;
+PLAYBOOK_str=$2;
+LOG_DIR_str=$3;
 ###ARGV
+
+###CFG
+INV_FILE_str="$SELF_DIR_str/lxc_server_hosts";
+CONF_DIVISIONS_FOR_INV_HOSTS_FILE_str="$SELF_DIR_str/01_configs/00_conf_divisions_for_inv_hosts";
+###CFG
 
 ###VARS
-NOW_DT=`date '+%Y%m%d%H%M%S'`;
-CUR_USER=`id -u -n`;
-LOG_FILE="$LOG_DIR/$NOW_DT-$CUR_USER.log";
+NOW_DT_str=`date '+%Y%m%d%H%M%S'`;
+CUR_USER_str=`id -u -n`;
+LOG_FILE_str="$LOG_DIR_str/$NOW_DT_str-$CUR_USER_str.log";
 ###VARS
 
 ###MAIN
-/usr/bin/mkdir -p "$LOG_DIR";
+/usr/bin/mkdir -p "$LOG_DIR_str";
 
-echo "User: $CUR_USER" | tee -a $LOG_FILE;
-echo "Used inventory file: $INV_FILE" | tee -a $LOG_FILE;
-echo "Used playbook: $SELF_DIR/playbooks/$PLAYBOOK" | tee -a $LOG_FILE;
-echo "Start time: $NOW_DT" | tee -a $LOG_FILE;
-echo "#########" | tee -a $LOG_FILE;
+echo "User: $CUR_USER_str" | tee -a $LOG_FILE_str;
+echo "Used inventory file: $INV_FILE_str" | tee -a $LOG_FILE_str;
+echo "Used limit for inventory file: $INV_LIMIT_str" | tee -a $LOG_FILE_str;
+echo "Used playbook: $SELF_DIR_str/playbooks/$PLAYBOOK_str" | tee -a $LOG_FILE_str;
+echo "Start time: $NOW_DT_str" | tee -a $LOG_FILE_str;
+echo "#########" | tee -a $LOG_FILE_str;
 
-/usr/bin/ansible-playbook -i $INV_FILE -u root --private-key=~/.ssh/id_rsa "$SELF_DIR/playbooks/$PLAYBOOK" | tee -a $LOG_FILE;
+/usr/bin/ansible-playbook -i $INV_FILE_str -u root --private-key=~/.ssh/id_rsa "$SELF_DIR_str/playbooks/$PLAYBOOK_str" | tee -a $LOG_FILE_str;
 ###MAIN
 
 
