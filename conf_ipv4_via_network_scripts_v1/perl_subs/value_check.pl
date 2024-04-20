cd8fb3f3a98368178e80258a7e686bb5043bb4e5
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-04-21 02:28:26 +0400

    202404210228

--- conf_ipv4_via_network_scripts_v1/perl_subs/value_check.pl
+++ conf_ipv4_via_network_scripts_v1/perl_subs/value_check.pl
@@ -1,6 +1,13 @@
 sub hwaddr_check {
     my ($hwaddr_l,$inv_hosts_network_data_href_l)=@_;
+    #inv_hosts_network_data_href_l=hash ref for %inv_hosts_network_data_g
+        #v1) key0='hwaddr_all', key1=hwaddr, value=inv_host
+        #v2) key0='inv_host', key1=inv_host, key2=interface_name, key3=hwaddr
+    my $proc_name_l=(caller(0))[3];
     
+    my $return_str_l='OK';
+    
+    return $return_str_l;
 }
 
 #With best regards
