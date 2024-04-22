ad5e27372b6ad9866fa9f114aedb2a157ff8e2ff
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-04-22 04:37:34 +0400

    202404220437

--- conf_ipv4_via_network_scripts_v1/perl_subs/value_check.pl
+++ conf_ipv4_via_network_scripts_v1/perl_subs/value_check.pl
@@ -1,5 +1,5 @@
 sub hwaddr_check {
-    my ($hwaddr_l,$inv_hosts_network_data_href_l)=@_;
+    my ($inv_host_l,$interface_name_l,$hwaddr_l,$inv_hosts_network_data_href_l)=@_;
     #inv_hosts_network_data_href_l=hash ref for %inv_hosts_network_data_g
         #v1) key0='hwaddr_all', key1=hwaddr, value=inv_host
         #v2) key0='inv_host', key1=inv_host, key2=interface_name, key3=hwaddr
@@ -8,8 +8,18 @@ sub hwaddr_check {
     my $return_str_l='OK';
 
     if ( $hwaddr_l!~/^\S{2}\:\S{2}\:\S{2}\:\S{2}\:\S{2}\:\S{2}$/ ) {
-        $return_str_l="fail [$proc_name_l]. HWADDR must be like 'XX:XX:XX:XX:XX:XX' (incorrect value='$hwaddr_l'). Please, check and correct config-file ('01a_conf_int_hwaddr_inf')";
+        $return_str_l="fail [$proc_name_l]. Incorrect value='$hwaddr_l' (inv-host='$inv_host_l', interface='$interface_name_l'). HWADDR must be like 'XX:XX:XX:XX:XX:XX'. Please, check and correct config-file ('01a_conf_int_hwaddr_inf')";
         return $return_str_l;
+    }
+
+    if ( exists(${$inv_hosts_network_data_href_l}{'hwaddr_all'}{$hwaddr_l}) && $inv_host_l ne ${$inv_hosts_network_data_href_l}{'hwaddr_all'}{$hwaddr_l} ) {
+        $return_str_l="fail [$proc_name_l]. NETWORK DATA check. HWADDR='$hwaddr_l' configured for inv_host='$inv_host_l' is already used by host='${$inv_hosts_network_data_href_l}{'hwaddr_all'}{$hwaddr_l}'. Please, check and correct config-file ('01a_conf_int_hwaddr_inf') or solve problem with duplicated mac-address";
+	return $return_str_l;
+    }
+                    
+    if ( !exists(${$inv_hosts_network_data_href_l}{'inv_host'}{$inv_host_l}{$interface_name_l}{$hwaddr_l}) ) {
+        $return_str_l="fail [$proc_name_l]. NETWORK DATA check. At inv_host='$inv_host_l' interface='interface_name_l' not linked with hwaddr='$hwaddr_l. Please, check and correct config-file ('01a_conf_int_hwaddr_inf')";
+	return $return_str_l;
     }
 
     return $return_str_l;
