7ee84b27a72c75307c723ae82f1eb0a46eef16b0
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-06-13 01:49:48 +0400

    202406130149

--- conf_ipv4_via_network_scripts_v2/perl_subs/value_check.pl
+++ conf_ipv4_via_network_scripts_v2/perl_subs/value_check.pl
@@ -259,7 +259,7 @@ sub prefix_ipv4_check {
     
     my $return_str_l='OK';
     
-    if ( $prefix_ipv4_l<0 or $prefix_ipv4_l>32 ) {
+    if ( $prefix_ipv4_l!~/^\d+$/ or $prefix_ipv4_l<0 or $prefix_ipv4_l>32 ) {
 	return "fail [$proc_name_l]. Incorrect prefix='$prefix_ipv4_l'. Prefix must be in the range from 0 to 32. Please, check and correct config-file ('$conf_file_l')";
     }
     
