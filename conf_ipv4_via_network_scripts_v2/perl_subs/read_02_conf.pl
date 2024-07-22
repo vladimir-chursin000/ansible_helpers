84fd954fb90878c42b4f949354dc594109cff8a9
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-07-22 23:24:55 +0400

    202407222324

--- conf_ipv4_via_network_scripts_v2/perl_subs/read_02_conf.pl
+++ conf_ipv4_via_network_scripts_v2/perl_subs/read_02_conf.pl
@@ -68,13 +68,17 @@ sub read_02_dns_settings {
     # check %res_tmp_lv0_l and fill %res_tmp_lv1_l (begin)
     while ( ($hkey0_l,$hval0_l)=each %res_tmp_lv0_l ) {
 	#hkey0_l=inv-host, hval0_l="search-domain=somedomain.org,nameserver1,nameserver2,etc" or "nameserver1,nameserver2,etc"
-	
+	if ( $hval0_l=~/\,search\-domain\=/ ) {
+	    
+	}
     }
     
     # clear vars
     ($hkey0_l,$hval0_l)=(undef,undef);
     ###
     # check %res_tmp_lv0_l and fill %res_tmp_lv1_l (end)
+    
+    if ( $return_str_l!~/^OK$/ ) { return $return_str_l; }
 
     # fill result hash
     %{$res_href_l}=%res_tmp_lv1_l;
