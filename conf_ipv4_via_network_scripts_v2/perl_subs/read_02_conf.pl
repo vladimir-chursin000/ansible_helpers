73751ac541f3b2320682b23d820081eb72eb7dd9
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-07-25 03:12:52 +0400

    202407250312

--- conf_ipv4_via_network_scripts_v2/perl_subs/read_02_conf.pl
+++ conf_ipv4_via_network_scripts_v2/perl_subs/read_02_conf.pl
@@ -53,12 +53,14 @@ sub read_02_dns_settings {
     my ($exec_res_l)=(undef);
     my ($hkey0_l,$hval0_l)=(undef,undef);
     my ($search_domain_l)=(undef);
+    my $cnt_l=0;
     my @nameservers_l=();
     my $return_str_l='OK';
     
     my %res_tmp_lv0_l=();
         #key=string with params from cfg, value=1
     my %res_tmp_lv1_l=(); # result hash
+	#key=inv_host, value=[search-domain(optional), array of nameservers]
     
     $exec_res_l=&read_conf_lines_with_priority_by_first_param($file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,2,0,\%res_tmp_lv0_l);
     #$file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,$needed_elements_at_line_arr_l,$add_ind4key_l,$res_href_l
@@ -76,13 +78,32 @@ sub read_02_dns_settings {
 	@nameservers_l=split(/\,/,$hval0_l);
 	
 	if ( $nameservers_l[0]=~/search\-domain\=/ ) {
-	    $nameservers_l[0]=~s/search\-domain\=//;
-	    $nameservers_l[0]='search '.$nameservers_l[0];
+	    $search_domain_l=$nameservers_l[0];
+	    $search_domain_l=~s/search\-domain\=//;
+	    $search_domain_l='search '.$nameservers_l[0];
+	    
+	    shift(@nameservers_l);
 	}
+	else { $search_domain_l='no'; }
+	
+	for ( $cnt_l=0; $cnt_l<=$#nameservers_l; $cnt_l++ ) { $nameservers_l[$cnt_l]='nameserver '.$nameservers_l[$cnt_l]; }
+	
+	if ( $search_domain_l!~/^no$/ ) { @nameservers_l=($search_domain_l,@nameservers_l); }
+	
+	# fill %res_tmp_lv1_l
+	$res_tmp_lv1_l{$hkey0_l}=[@nameservers_l];
+	###
+	
+	# clear vars
+	@nameservers_l=();
+	$search_domain_l=undef;
+	$cnt_l=0;
+	###
     }
     
     # clear vars
     ($hkey0_l,$hval0_l)=(undef,undef);
+    $cnt_l=undef;
     ###
     # check %res_tmp_lv0_l and fill %res_tmp_lv1_l (end)
     
