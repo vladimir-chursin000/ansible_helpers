5c6c2dc5d53cc2f02d77ec68769c6b973936afe4
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-10-12 22:55:27 +0400

    202410122255

--- conf_ipv4_via_network_scripts_v2/perl_subs/read_03_conf.pl
+++ conf_ipv4_via_network_scripts_v2/perl_subs/read_03_conf.pl
@@ -1,5 +1,12 @@
 sub read_03_conf_routes {
     my ($file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,$res_href_l)=@_;
+    #$file_l=$f03_conf_routes_path_g
+    #inv_hosts_href_l=hash-ref for %inventory_hosts_g
+    #$divisions_for_inv_hosts_href_l=hash-ref for %h00_conf_divisions_for_inv_hosts_hash_g
+        #$h00_conf_divisions_for_inv_hosts_hash_g{group-name}{inv-host}=1;
+    #$res_href_l=hash ref for %h03_conf_routes_hash_g
+	#key=inv-host, value=[array of routes]
+    ###############
 }
 
 #With best regards
