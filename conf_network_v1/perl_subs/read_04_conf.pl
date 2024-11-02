4bcac763fe4c5264497eec8657c45cf2cb91d876
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-11-02 23:32:11 +0400

    202411022332

--- conf_network_v1/perl_subs/read_04_conf.pl
+++ conf_network_v1/perl_subs/read_04_conf.pl
@@ -12,5 +12,16 @@ sub read_04_conf_remote_backend {
         
     my ($exec_res_l)=(undef);
     my ($hkey0_l,$hval0_l)=(undef,undef);
+    my $return_str_l='OK';
+
+    my %res_tmp_lv0_l=();
+        #key=string with params from cfg, value=1
+    my %res_tmp_lv1_l=(); # result hash
+        #key=inv_host, value=
+
+    $exec_res_l=&read_conf_lines_with_priority_by_first_param($file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,2,0,\%res_tmp_lv0_l);
+    #$file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,$needed_elements_at_line_arr_l,$add_ind4key_l,$res_href_l
+    if ( $exec_res_l=~/^fail/ ) { return "fail [$proc_name_l] -> ".$exec_res_l; }
+    $exec_res_l=undef;
 
 }
