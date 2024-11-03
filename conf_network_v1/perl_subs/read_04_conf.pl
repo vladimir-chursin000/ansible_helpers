ee358cc8b0f7ace1027e26514366e7042eca357c
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-11-03 22:59:26 +0400

    202411032259

--- conf_network_v1/perl_subs/read_04_conf.pl
+++ conf_network_v1/perl_subs/read_04_conf.pl
@@ -5,7 +5,7 @@ sub read_04_conf_remote_backend {
     #$divisions_for_inv_hosts_href_l=hash-ref for %h00_conf_divisions_for_inv_hosts_hash_g
         #$h00_conf_divisions_for_inv_hosts_hash_g{group-name}{inv-host}=1;
     #$res_href_l=hash ref for %h04_conf_remote_backend_hash_g
-        #Key=inv_host, value=
+        #Key=inv_host, value=network-scripts/NetworkManager
     ###############
 
     my $proc_name_l=(caller(0))[3];
@@ -17,7 +17,7 @@ sub read_04_conf_remote_backend {
     my %res_tmp_lv0_l=();
         #key=string with params from cfg, value=1
     my %res_tmp_lv1_l=(); # result hash
-        #key=inv_host, value=
+        #key=inv_host, value=network-scripts/NetworkManager
 
     $exec_res_l=&read_conf_lines_with_priority_by_first_param($file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,2,0,\%res_tmp_lv0_l);
     #$file_l,$inv_hosts_href_l,$divisions_for_inv_hosts_href_l,$needed_elements_at_line_arr_l,$add_ind4key_l,$res_href_l
