66e8f021000cfa5d74921dc5f537ed3efa82a837
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2024-12-01 21:28:11 +0400

    202412012128

--- conf_network_v1/generate_json_files.pl
+++ conf_network_v1/generate_json_files.pl
@@ -58,7 +58,7 @@ our $f05_not_configured_interfaces_path_g=$self_dir_g.'/01_configs/05_not_config
 our $f06_conf_temp_apply_path_g=$self_dir_g.'/01_configs/06_conf_temp_apply';
 ###CFG file (end)
 
-############STATIC VARS. Change dir paths if you want just use this script without ansible helper (begin)
+############STATIC VARS (begin)
 our $ifcfg_backup_from_remote_dir_g=$self_dir_g.'/playbooks/ifcfg_backup_from_remote/now'; # dir contains actual ifcfg-files downloaded from remote hosts with help of playbook 'ifcfg_backup_playbook.yml' before run this script
 our $ifcfg_backup_from_remote_nd_file_g=$self_dir_g.'/playbooks/ifcfg_backup_from_remote/network_data/inv_hosts_interfaces_info.txt'; # dir contains actual network_data (eth, hwaddr) downloaded from remote hosts with help of playbook 'ifcfg_backup_playbook.yml' before run this script
 our $remote_dir_for_absible_helper_g='~/ansible_helpers/conf_int_ipv4_via_network_scripts'; # dir for creating/manipulate files at remote side
