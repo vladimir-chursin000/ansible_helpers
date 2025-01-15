a01ff678b997ef0924206f373ed0a02c360723e3
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2025-01-15 23:46:34 +0400

    202501152346

--- conf_network_v1/00_install_network_backend_n_conf_network.sh
+++ conf_network_v1/00_install_network_backend_n_conf_network.sh
@@ -4,7 +4,7 @@
 # Do not change this variables manually
 SELF_DIR_str="$(dirname $(readlink -f $0))";
 INV_LIMIT_str='no';
-PLAYBOOK_str='full_install_network_scripts_and_configure_network_playbook.yml';
+PLAYBOOK_str='full_install_n_configure_network_playbook.yml';
 LOG_DIR_str="$SELF_DIR_str/run_history";
 PLAYBOOK_BEFORE_str='conf_backup_playbook.yml';
 GEN_DYN_FILES_RUN_str='yes';
