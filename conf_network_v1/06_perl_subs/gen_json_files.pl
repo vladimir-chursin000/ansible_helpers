07b35e2d16f1aba8eb90467d060a2ad3f5c3c7ed
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2025-01-30 23:37:42 +0400

    202501302337

--- conf_network_v1/06_perl_subs/gen_json_files.pl
+++ conf_network_v1/06_perl_subs/gen_json_files.pl
@@ -1,5 +1,5 @@
 sub generate_json_conf_files {
-    my ($h01a_conf_int_hwaddr_inf_href_l,$h01b_conf_main_href_l)=@_;
+    my ($h01a_conf_int_hwaddr_inf_href_l,$h01b_conf_main_href_l,$h01c_conf_ip_addr_href_l,$h01d_conf_bond_opts_href_l)=@_;
     
     my $proc_name_l=(caller(0))[3];
     
