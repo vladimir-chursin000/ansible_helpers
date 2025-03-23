79712cae0238500b434225c8017c3cb09ca7462d
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2025-03-23 23:44:55 +0400

    202503232344

--- conf_network_v1/06_perl_subs/gen_json_files.pl
+++ conf_network_v1/06_perl_subs/gen_json_files.pl
@@ -2,15 +2,32 @@ sub generate_json_conf_files {
     my ($tmp_prms_href_l)=@_;
     
     my $proc_name_l=(caller(0))[3];
-
+    
+    my $h01a_conf_int_hwaddr_inf_href_l=${$tmp_prms_href_l}{'h01a_conf_int_hwaddr_inf_href'};
     #'h01a_conf_int_hwaddr_inf_href'=>\%h01a_conf_int_hwaddr_inf_hash_g,
+    
+    my $h01b_conf_main_href_l=${$tmp_prms_href_l}{'h01b_conf_main_href'};
     #'h01b_conf_main_href'=>\%h01b_conf_main_hash_g,
+    
+    my $h01c_conf_ip_addr_href_l=${$tmp_prms_href_l}{'h01c_conf_ip_addr_href'};
     #'h01c_conf_ip_addr_href'=>\%h01c_conf_ip_addr_hash_g,
+    
+    my $h01d_conf_bond_opts_href_l=${$tmp_prms_href_l}{'h01d_conf_bond_opts_href'};
     #'h01d_conf_bond_opts_href'=>\%h01d_conf_bond_opts_hash_g,
+    
+    my $h02_conf_dns_href_l=${$tmp_prms_href_l}{'h02_conf_dns_href'};
     #'h02_conf_dns_href'=>\%h02_conf_dns_hash_g,
+    
+    my $h03_conf_routes_href_l=${$tmp_prms_href_l}{'h03_conf_routes_href'};
     #'h03_conf_routes_href'=>\%h03_conf_routes_hash_g,
+    
+    my $h04_conf_remote_backend_href_l=${$tmp_prms_href_l}{'h04_conf_remote_backend_href'};
     #'h04_conf_remote_backend_href'=>\%h04_conf_remote_backend_hash_g,
-    #'h05_not_configured_interfaces_href'=>\%h05_not_configured_interfaces_hash_g,
+    
+    my $h01a_conf_int_hwaddr_inf_href_l=${$tmp_prms_href_l}{'h01a_conf_int_hwaddr_inf_href'};
+    #'h01a_conf_int_hwaddr_inf_href'=>\%h05_not_configured_interfaces_hash_g,
+    
+    my $h06_conf_temp_apply_href_l=${$tmp_prms_href_l}{'h06_conf_temp_apply_href'};
     #'h06_conf_temp_apply_href'=>\%h06_conf_temp_apply_hash_g,
     
     my $return_str_l='OK';
