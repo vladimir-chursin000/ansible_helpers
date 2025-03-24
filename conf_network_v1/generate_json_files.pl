8994e0557a2b277e9d4d5d2a4c239b28a86ba86b
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2025-03-24 23:50:31 +0400

    202503242350

--- conf_network_v1/generate_json_files.pl
+++ conf_network_v1/generate_json_files.pl
@@ -69,6 +69,7 @@ our $f06_conf_temp_apply_path_g=$self_dir_g.'/01_configs/06_conf_temp_apply';
 ############STATIC VARS (begin)
 our $ifcfg_backup_from_remote_dir_g=$self_dir_g.'/02_files/conf_backup_from_remote/now'; # dir contains actual ifcfg-files downloaded from remote hosts with help of playbook 'ifcfg_backup_playbook.yml' before run this script
 our $ifcfg_backup_from_remote_nd_file_g=$self_dir_g.'/02_files/conf_backup_from_remote/network_data/inv_hosts_interfaces_info.txt'; # dir contains actual network_data (eth, hwaddr) downloaded from remote hosts with help of playbook 'ifcfg_backup_playbook.yml' before run this script
+our $generated_json_files_dir_g='';
 ############STATIC VARS (end)
 
 ############VARS (begin)
