6d4d0c7140dc482f96af43fec1e156937fc070e5
Vladimir Chursin <vladimir.chursin000@yandex.ru>
2025-04-14 23:57:41 +0400

    202504142357

--- conf_network_v1/06_perl_subs/gen_json_files.pl
+++ conf_network_v1/06_perl_subs/gen_json_files.pl
@@ -47,16 +47,19 @@ sub generate_json_conf_files {
     
     ###json network-interface structure descr (begin)
     #{
+    	
     #}
     ###json network-interface structure descr (end)
-
+    
     ###json dns-conf structure descr (begin)
     #{
+    	
     #}
     ###json dns-conf structure descr (end)
-
+    
     ###json routes-conf structure descr (begin)
     #{
+    	
     #}
     ###json routes-conf structure descr (end)
     
